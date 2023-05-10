38e5211edf166429149a6a3a1e1c4921e5f41a5c
ZerrY <suhan0304@gmail.com>
2023-05-10 16:42:55 +0900

    Update 1005.py

--- 1005.py
+++ 1005.py
@@ -1,20 +1,33 @@
-T = int(input())
-for i in range(T) :
-    N, K = map(int,input().split())
-    time = [-1] + list(map(int, input().split()))
+import sys
+from collections import deque
 
-    worst_time = [-1]
-    for i in range(1,len(time)) :
-        worst_time.append(time[i])
+T=int(sys.stdin.readline())
 
-    rules = list()
-    for j in range(K) :
-        X, Y = map(int, input().split())
-        rules.append([X,Y])
-    rules.sort()
-    for j in range(K) :
-        worst_time[rules[j][1]] = max(worst_time[rules[j][1]], worst_time[rules[j][0]] + time[rules[j][1]])
-    
-    print(worst_time)
-    W = int(input())
-    print(worst_time[W])
+for _ in range(T) :
+    N, K = map(int, sys.stdin.readline().split())
+    time = [0] + list(map(int, sys.stdin.readline().split()))
+    seq = [[] for _ in range(N+1)]
+    inDegree = [0 for _ in range(N+1)]
+    DP = [0 for _ in range(N+1)]
+
+    for _ in range(K) :
+        a,b = map(int, sys.stdin.readline().split())
+        seq[a].append(b)
+        inDegree[b] += 1
+
+    q = deque()
+    for i in range(1, N+1) :
+        if inDegree[i] == 0 :
+            q.append(i)
+            DP[i]=time[i]
+
+    while q :
+        a = q.popleft()
+        for i in seq[a] :
+            inDegree[i] -= 1
+            DP[i] = max(DP[a] + time[i], DP[i])
+            if inDegree[i] == 0:
+                q.append(i)
+
+    ans = int(sys.stdin.readline())
+    print(DP[ans])
