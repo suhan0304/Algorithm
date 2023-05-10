19b98fcf1cda69ca366d539e17913e7ee141ddb6
ZerrY <suhan0304@gmail.com>
2023-05-10 15:15:11 +0900

    baekjoon

--- 1002.py
+++ 1002.py
@@ -1,5 +1,13 @@
+import math
 n = input()
-for i in range(n) :
-    x1, y1, r1, x2, y2, r2 = input().split()
-    r3 = (int(x1)-int(x2))**2 + (int(y1)-int(y2)**2)
-    print(r3)
+for i in range(int(n)) :
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    r = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+    if r == 0 and r1 == r2 :
+        print(-1)
+    elif abs(r1-r2) == r or r1+r2 == r:
+        print(1)
+    elif abs(r1-r2) < r < (r1+r2) :
+        print(2)
+    else :
+        print(0)
