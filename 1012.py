21fe17050b4f54e2e99655b4c01b8f026ff0c1d9
ZerrY <suhan0304@gmail.com>
2023-10-30 21:17:09 +0900

    1012

--- 1012.py
+++ 1012.py
@@ -2,3 +2,7 @@ T = int(input())
 for i in range(T) :
     N, M, K = map(int, input().split())
     field = [[0] * M]* N
+    for j in range(K) :
+        y, x = map(int, input().split())
+        field[x][y] = 0
+    print(field)
