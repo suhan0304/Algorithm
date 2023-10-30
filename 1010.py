494376694f3831de84ba5e9fb7991c43f703f773
ZerrY <suhan0304@gmail.com>
2023-10-30 20:49:59 +0900

    1016

--- 1010.py
+++ 1010.py
@@ -1,10 +1,13 @@
 def factorial(n):
-    if n == 1:      # n이 1일 때
+    if n == 0 or n == 1:      # n이 1일 때
         return 1    # 1을 반환하고 재귀호출을 끝냄
     return n * factorial(n - 1) 
 
 T = int(input())
 ans = []
 for i in range(T) :
-    m, n = map(input().split(), int)
+    m, n = map(int, input().split())
     ans.append(factorial(n) / (factorial(n-m) * factorial(m)))
+
+for i in range(T) :
+    print(int(ans[i]))
--- 1016.py
+++ 1016.py
@@ -10,7 +10,6 @@ for i in range(2, int(max**0.5+1)):
         #min보다 크면서 가장 작은 square의 배수를 시작지점을 맞춰주기 위한 for문
         #ex) 10부터 100까지라고 한다면 
         #   4의 배수를 12부터 삭제해나가기 시작 12, 16, 20, ...
-        
         #   9의 배수를 18부터 삭제해나가기 시작 18, 27, ...
         if not divisibleByTheSquare[j-min] :
             divisibleByTheSquare[j-min] = True
