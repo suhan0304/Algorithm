052b3d87d66bd6add84e6f9e281253267b6ea6fb
ZerrY <suhan0304@gmail.com>
2023-10-31 20:46:28 +0900

    1012

--- 1012.py
+++ 1012.py
@@ -1,40 +1,40 @@
-def findCabbage(x, y):
-    if ground[x][y] == 1:
-        ground[x][y] = -1
+import sys
+
+sys.setrecursionlimit(10**5)
+
+
+def findCabbage(y, x):
+    if ground[y][x] == 1:
+        ground[y][x] = 0
         # 상하좌우 순서대로 배추가 있는지 확인 후 0으로 만든다
-        if x > 0 and ground[x - 1][y] == 1:
-            findCabbage(x - 1, y)
-        elif x < len(ground) - 1 and ground[x + 1][y] == 1:
-            findCabbage(x + 1, y)
-        elif y > 0 and ground[x][y - 1] == 1:
-            findCabbage(x + 1, y)
-        elif y < len(ground[0]) - 1 and ground[x][y + 1] == 1:
-            findCabbage(x, y + 1)
+        if y > 0 and ground[y - 1][x] == 1:
+            findCabbage(y - 1, x)
+        elif y < N - 1 and ground[y + 1][x] == 1:
+            findCabbage(y + 1, x)
+        elif x > 0 and ground[y][x - 1] == 1:
+            findCabbage(y, x - 1)
+        elif x < M - 1 and ground[y][x + 1] == 1:
+            findCabbage(y, x + 1)
         return
 
 
 ans = []
 T = int(input())
 for i in range(T):
-    N, M, K = map(int, input().split())
-    ground = [[0 for _ in range(N)] for __ in range(M)]
+    M, N, K = map(int, input().split())
+    ground = [[0 for _ in range(M)] for __ in range(N)]
     for j in range(K):
-        y, x = map(int, input().split())
-        ground[x][y] = 1
+        x, y = map(int, input().split())
+        ground[y][x] = 1
 
     cnt = 0
-    """
-    for x in range(N - 1):
-        for y in range(M - 1):
-            if ground[x][y] == 0:
-                continue
-            else:
+    for y in range(N):
+        for x in range(M):
+            if ground[y][x] == 1:
                 cnt += 1
-                findCabbage(x, y)
+                findCabbage(y, x)
 
     ans.append(cnt)
 
-    for j in ground:
-        print(j)
-
-    """
+for answer in ans:
+    print(answer)
