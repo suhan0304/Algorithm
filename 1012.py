e70d3ca9259514d301a4de43891f82aca1abb415
ZerrY <suhan0304@gmail.com>
2023-10-31 21:29:12 +0900

    1012

--- 1012.py
+++ 1012.py
@@ -3,25 +3,29 @@ import sys
 sys.setrecursionlimit(10**5)
 
 
-def dfs(y, x, ground, m, n):
-    ground[y][x] = 0
-    if 0 <= y - 1 and ground[y - 1][x] == 1:
-        dfs(y - 1, x, ground, m, n)
-    elif y + 1 < m and ground[y + 1][x] == 1:
-        dfs(y + 1, x, ground, m, n)
-    elif 0 <= x - 1 and ground[y][x - 1] == 1:
-        dfs(y, x - 1, ground, m, n)
-    elif x + 1 < n and ground[y][x + 1] == 1:
-        dfs(y, x + 1, ground, m, n)
-    return
+def dfs(y1, x1, ground, m, n):
+    ground[y1][x1] = 0
+
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
+
+    for i in range(4):
+        y2 = y1 + dy[i]
+        x2 = x1 + dx[i]
+        if y2 < 0 or m <= y2 or x2 < 0 or n <= x2:
+            continue
+
+        if ground[y2][x2] == 1:
+            dfs(y2, x2, ground, m, n)
 
 
 for _ in range(int(input())):
     ans = 0
+
     arr = []
     m, n, k = map(int, input().split())
     ground = [[0 for _ in range(n)] for __ in range(m)]
-    for j in range(k):
+    for _ in range(k):
         x, y = map(int, input().split())
         ground[x][y] = 1
         arr.append([x, y])
