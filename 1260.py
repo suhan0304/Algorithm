657b9094463a00c9a5e32d44eaf4b5f3aa9b24cc
ZerrY <suhan0304@gmail.com>
2023-11-01 00:04:26 +0900

    1260

--- 1260.py
+++ 1260.py
@@ -1,12 +1,39 @@
-def dfs():
+def dfs(graph, v):
+    graph[v][v] = -1  # 방문한 노드는 방문했다고 표시
+    print(v, end=" ")
+    for i in range(1, n + 1):
+        # 간선이 있고, 해당 노드에 방문학 적이 없다면
+        if graph[v][i] == 1 and graph[i][i] != -1:
+            dfs(graph, i)  # 재귀를 사용해 dfs 구현
     return
 
 
-def bfs():
+def bfs(graph, v):
+    queue = []  # bfs는 큐를 이용해서 쉽게 구현
+    queue.append(v)
+    graph[v][v] = -1  # 방문한 노드는 방문했다고 표시
+    while queue:  # 원소가 하나라도 있을시 True, 원소가 하나도 없으면 False
+        v = queue.pop(0)
+        print(v, end=" ")
+        for i in range(1, n + 1):
+            # 간선이 있고, 해당 노드에 방문학 적이 없다면
+            if graph[v][i] == 1 and graph[i][i] != -1:
+                queue.append(i)
+                graph[i][i] = -1  # 방문한 노드는 방문했다고 표시
     return
 
 
 n, m, v = map(int, input().split())
 
-for _ in range(v):
+graph = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+for _ in range(m):
     v1, v2 = map(int, input().split())
+    graph[v1][v2] = graph[v2][v1] = 1
+
+dfs(graph, v)
+
+for i in range(1, n + 1):
+    graph[i][i] = 0
+
+print()
+bfs(graph, v)
