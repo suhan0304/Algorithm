2201bb7a77c14a50a6300a5a968ec36c4523d1a7
ZerrY <suhan0304@gmail.com>
2023-10-31 22:44:26 +0900

    1018

---
+++ 1018.py
@@ -0,0 +1,34 @@
+N, M = map(int, input().split())
+
+chessboard = []
+for _ in range(N):
+    chessboard.append(input())
+
+chg_w = [[0 for _ in range(M)] for _ in range(N)]
+chg_b = [[0 for _ in range(M)] for _ in range(N)]
+
+for i in range(N):
+    for j in range(M):
+        # i + j가 짝수인 칸
+        if (i + j) % 2 == 0:
+            if chessboard[i][j] == "B":
+                chg_w[i][j] = 1
+            else:
+                chg_b[i][j] = 1
+        # i + j가 짝수인 칸
+        elif (i + j) % 2 == 1:
+            if chessboard[i][j] == "W":
+                chg_w[i][j] = 1
+            else:
+                chg_b[i][j] = 1
+
+ans = []
+for i in range(N - 8 + 1):
+    for j in range(M - 8 + 1):
+        s_w = s_b = 0
+        for k in range(8):
+            s_w += sum(chg_w[i + k][j : j + 8])
+            s_b += sum(chg_b[i + k][j : j + 8])
+        ans.append(min(s_w, s_b))
+
+print(min(ans))
