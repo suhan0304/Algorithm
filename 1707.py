8805cad90841616f541f0928616ff834e15d9818
ZerrY <suhan0304@gmail.com>
2023-11-01 20:15:53 +0900

    1707.py

--- 1707.py
+++ 1707.py
@@ -4,18 +4,44 @@ sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 
-def dfs(graph, v, visited):
-    visited[v] = True
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
-    return
+def dfs(graph, v, visited, b, bit):
+    if bit[0] == -1:
+        return
+    else:
+        visited[v] = True
+        # print(bit)
+        bit[v] = b
+        for i in graph[v]:
+            if not visited[i]:
+                dfs(graph, i, visited, b * -1, bit)
+            else:
+                if bit[i] == bit[v]:
+                    bit[0] = -1
+                    return
+        return
 
 
-for ___ in range(int(input())):
-    v, e = map(int, input().split())
-    graph = [[] for _ in range(v + 1)]
-    for i in range(e):
+ans = []
+T = int(input())
+for ___ in range(T):
+    V, E = map(int, input().split())
+    graph = [[] for _ in range(V + 1)]
+    for i in range(E):
         u, v = map(int, input().split())
         graph[u].append(v)
         graph[v].append(u)
+
+    cnt = 0
+    visited = [False] * (V + 1)
+    bit = [0] * (V + 1)
+    for i in range(1, V + 1):
+        if not visited[i]:
+            flag = 0
+            dfs(graph, i, visited, 1, bit)
+    if bit[0] != -1:
+        ans.append("YES")
+    else:
+        ans.append("NO")
+
+for l in ans:
+    print(l)
