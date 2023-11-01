877f9c9c1f5a634059da96508805b915be29c1c2
ZerrY <suhan0304@gmail.com>
2023-11-01 14:45:43 +0900

    11724

--- 11724.py
+++ 11724.py
@@ -1,22 +1,28 @@
-def dfs(graph, v):
+import sys
+
+sys.setrecursionlimit(10**6)
+
+
+def dfs(graph, v, visited):
     visited[v] = True
-    for i in range(1, n + 1):
-        if graph[v][i] == 1 and visited[i] == False:
-            dfs(graph, i)
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
     return
 
 
 n, m = map(int, input().split())
 graph = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
 for i in range(m):
-    v1, v2 = map(int, input().split())
-    graph[v1][v2] = graph[v2][v1] = 1
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
 cnt = 0
 visited = [False for _ in range(n + 1)]
 for i in range(1, n + 1):
-    if visited[i] == False:
-        dfs(graph, i)
+    if not visited[i]:
+        dfs(graph, i, visited)
         cnt += 1
 
 print(cnt)
