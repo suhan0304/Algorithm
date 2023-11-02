91e48439835e2df010a9151f71bf145b5e4a53c9
ZerrY <suhan0304@gmail.com>
2023-11-02 20:41:14 +0900

    1052.py

---
+++ 1052.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+n 
+
+i = 0
+while i < k:
+    m = 1
+    while n - m < 0:
+        if :
+            break
+        if n - a[i] > 0:
+            n = n - a[i]
+            a[i] *= 2
+    i += 1
+
+print(abs(a[i - 1] - n))
