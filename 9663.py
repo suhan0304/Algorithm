1344119e9a2cd01692a2d765bbc076df6d8384fa
ZerrY <suhan0304@gmail.com>
2023-11-02 22:51:46 +0900

    9662

--- 9663.py
+++ 9663.py
@@ -1,29 +1,26 @@
-import sys
+n = int(input())
+ans = 0
+board = [0] * n
 
 
 def promising(x):
     for i in range(x):
-        if board[x] == board[i]:
-            return 0
-        elif abs(x - i) == abs(board[x] - board[i]):
-            return 0
-    return 1
+        if board[x] == board[i] or abs(x - i) == abs(board[x] - board[i]):
+            return False
+    return True
 
 
 def nqueen(x):
     global ans
-    if x == N:
+    if x == n:
         ans += 1
         return
     else:
-        for i in range(N):
+        for i in range(n):
             board[x] = i  # [x, i]에 퀸을 위치
             if promising(x):
                 nqueen(x + 1)
 
 
-N = int(sys.stdin.readline())
-ans = 0
-board = [0 for _ in range(N)]
 nqueen(0)
 print(ans)
