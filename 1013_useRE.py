eb24bef81f33c0aaa1e9b7b1b9a992f7a353d644
ZerrY <suhan0304@gmail.com>
2023-11-04 13:57:58 +0900

    1013

--- 1013_useRE.py
+++ 1013_useRE.py
@@ -4,6 +4,9 @@ import re
 input = sys.stdin.readline
 
 T = int(input())
+
+import re
+
 pattern = r"(100+1+|01)+"
 for _ in range(T):
     print("YES" if re.fullmatch(pattern, input()[:-1]) else "NO")
