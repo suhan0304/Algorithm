6f7b5f836cd9446d8cd31f4f341c55d1b91513b7
ZerrY <suhan0304@gmail.com>
2023-11-04 12:25:47 +0900

    1013

--- 1013.py
+++ 1013.py
@@ -9,18 +9,26 @@ from collections import deque
 
 ans = True
 for _ in range(T):
+    ans = True
     q = deque(input()[:-1])
     while len(q) >= 2:
         if len(q) >= 3 and q[0] == "1" and q[1] == "0" and q[2] == "0":
             for _ in range(3):
                 q.popleft()
-            while q[0] == "0":
+            while q and q[0] == "0":
                 q.popleft()
             if len(q) >= 3 and q[0] == "1" and q[1] == "0" and q[2] == "0":
                 ans = False
                 break
             elif q and q[0] == "1":
-                while q and q[0] == "1":
+                while (
+                    q
+                    and q[0] == "1"
+                    and len(q) <= 2
+                    or q
+                    and q[0] == "1"
+                    and not (q[0] == "1" and q[1] == "0" and q[2] == "0")
+                ):
                     q.popleft()
             else:
                 ans = False
