ea1a1d4219b834783718c4413db6146a66f96a7d
ZerrY <suhan0304@gmail.com>
2023-11-04 13:07:31 +0900

    1059

--- 1059.py
+++ 1059.py
@@ -7,6 +7,7 @@ s = list(map(int, input().split()))
 n = int(input())
 s.sort()
 
+"""
 if n in s:
     print(0)
 else:
@@ -17,6 +18,26 @@ else:
             left = x
         elif x > n and right == 0:
             right = x
-    right -= 1
     left += 1
+    right -= 1
     print((n - left) * (right - n + 1) + (right - n))
+"""
+
+
+def proc(l, r):
+    return int((r - l + 1) * (r - l) / 2)
+
+
+if n in s:
+    print(0)
+else:
+    left = 0
+    right = 0
+    for x in s:
+        if x < n:
+            left = x
+        elif x > n and right == 0:
+            right = x
+    left += 1
+    right -= 1
+    print(proc(left, right) - proc(left, n - 1) - proc(n + 1, right))
