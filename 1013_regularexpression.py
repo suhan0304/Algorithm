78094e542d4ed8e5a2d0e5cfd06254e58453be5b
ZerrY <suhan0304@gmail.com>
2023-11-04 12:32:58 +0900

    1013

---
+++ 1013_regularexpression.py
@@ -0,0 +1,9 @@
+import sys
+import re
+
+input = sys.stdin.readline
+
+T = int(input())
+pattern = r"(100+1+|01)+"
+for _ in range(T):
+    print("YES" if re.fullmatch(pattern, input()[:-1]) else "NO")
