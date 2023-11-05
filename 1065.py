bf2c419cc6b756fecdc66de505f45cc8c2098eb4
ZerrY <suhan0304@gmail.com>
2023-11-06 03:32:09 +0900

    1065

---
+++ 1065.py
@@ -0,0 +1,16 @@
+import sys
+
+x = int(sys.stdin.readline())
+
+
+def hansu(x):
+    x = str(x)
+    if len(x) <= 2:
+        return True
+    else:
+        return True
+
+
+cnt = 0
+for i in range(x):
+    hansu(x)
