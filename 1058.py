31ed7a4c631b80358da85254e8a40224f7047b12
ZerrY <suhan0304@gmail.com>
2023-11-05 22:23:32 +0900

    1058

--- 1058.py
+++ 1058.py
@@ -1,41 +1,22 @@
 import sys
 from collections import deque
 
-input = sys.stdin.readline
+n = int(sys.stdin.readline())
+graph = [list(sys.stdin.readline().strip()) for _ in range(n)]
+# 2-친구 사이일 때 1, 아니면 0
+f = [[0] * n for _ in range(n)]
 
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            # 2-친구인 경우
+            if graph[i][j] == "Y" or (graph[i][k] == "Y" and graph[k][j] == "Y"):
+                f[i][j] = 1
 
-def bfs(graph, v, visited):
-    cnt = 0
-    queue = deque([v])
-    visited[v] = True
-    while queue and cnt < 2:
-        v = queue.popleft()
-        for i in graph[v]:
-            # 간선이 있고, 해당 노드에 방문학 적이 없다면
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
-        cnt += 1
-    return
+res = 0
 
-
-n = int(input())
-graph = [[] for _ in range(n)]
-
-for i in range(n):
-    s = input()[:-1]
-    for j in range(i + 1, len(s)):
-        if s[j] == "Y":
-            graph[i].append(j)
-            graph[j].append(i)
-
-ans = 0
-for i in range(n):
-    # bfs 방식으로 그냥 깊이가 2일때까지 방문한 노드의 수
-    # -> 2-친구의 수
-    visited = [False] * n
-    bfs(graph, i, visited)
-    print(visited, visited.count(True) - 1)
-    if ans < visited.count(True) - 1:
-        ans = visited.count(True) - 1
-print(ans)
+for row in f:
+    res = max(res, sum(row))
+print(res)
