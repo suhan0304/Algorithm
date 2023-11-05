d7b18299633e0c70eae0832349fc18800dd30a7c
ZerrY <suhan0304@gmail.com>
2023-11-05 15:01:38 +0900

    1068

--- 1068.py
+++ 1068.py
@@ -5,22 +5,27 @@ input = sys.stdin.readline
 
 def dfs(graph, v, visited):
     visited[v] = True
+    if not graph[v] and v != -1:
+        leaf.append(v)
+        return
     for i in graph[v]:
         if not visited[i]:
             dfs(graph, i, visited)
-    print(v, end=" ")
     return
 
 
 n = int(input())
 nodes = list(map(int, input().split()))
-
+erase_node = int(input())
 graph = [[] for _ in range(n + 1)]
 for i in range(len(nodes)):
-    graph[i].append(nodes[i])
-    graph[nodes[i]].append(i)
+    if i != erase_node:
+        graph[nodes[i]].append(i)
 
-cnt = 0
+
+leaf = []
 visited = [False] * (n + 1)
 dfs(graph, -1, visited)
-print(cnt)
+# print(leaf)
+
+print(len(leaf))
