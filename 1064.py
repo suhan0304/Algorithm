965fa6c8315178462ec41d97238cf97bb1ac4fcf
ZerrY <suhan0304@gmail.com>
2023-11-06 23:12:40 +0900

    1064

--- 1064.py
+++ 1064.py
@@ -3,3 +3,9 @@ import sys
 input = sys.stdin.readline
 
 xa, ya, xb, yb, xc, yc = map(int, input().split())
+
+# D의 좌표를 구하지 않아도 둘레는 구할 수 있음
+# 1. AC + CB * 2
+# 2. AB + BC * 2
+# 3. CA + CB * 2
+# 두점의 거리를 구하는 함수를 만들어 쉽게 해결
