b47bdbd8a6d9e7192f2e95918d3395cfb8a65861
ZerrY <suhan0304@gmail.com>
2023-11-07 23:00:45 +0900

    1072

--- 1072.py
+++ 1072.py
@@ -11,10 +11,12 @@ else:
     # 1씩 했더니 시간초과 그럼 이분탐색 해보자
     l = y
     r = x
+    z = int(l / r * 100)
     mid = 0
     while l <= r:
         mid = (l + r) // 2
-        z2 = int(l / r * 100)
+        z2 = int(r / l * 100)
+        print(l, mid, r, z, z2)
         if z + 1 == z2:
             break
         elif z + 1 < z2:
