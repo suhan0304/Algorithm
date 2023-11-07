27166fe9659e39a45eee56ecd13e5a832364ad0a
ZerrY <suhan0304@gmail.com>
2023-11-07 23:04:23 +0900

    1072

--- 1072.py
+++ 1072.py
@@ -15,7 +15,7 @@ else:
     mid = 0
     while l <= r:
         mid = (l + r) // 2
-        z2 = int(r / l * 100)
+        z2 = int(l / r * 100)
         print(l, mid, r, z, z2)
         if z + 1 == z2:
             break
