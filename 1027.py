37756f4aa5ae6687cb8cd17465f8e76796c3325b
ZerrY <suhan0304@gmail.com>
2023-11-08 13:53:06 +0900

    1027

--- 1027.py
+++ 1027.py
@@ -5,11 +5,36 @@ input = sys.stdin.readline
 N = int(input())
 M = list(map(int, input().split()))
 
+MAX = 1000000001
+
 
 def check(n):
-    return
+    left_max = MAX
+    right_max = MAX
+
+    cnt = 0
+    for i in range(n - 1, -1, -1):
+        if left_max == MAX:
+            left_max = (M[n] - M[i]) / (n - i)
+            cnt += 1
+        else:
+            if (M[n] - M[i]) / (n - i) < left_max:
+                left_max = (M[n] - M[i]) / (n - i)
+                cnt += 1
+
+    for i in range(n + 1, len(M)):
+        if right_max == MAX:
+            right_max = (M[n] - M[i]) / (n - i)
+            cnt += 1
+        else:
+            if (M[i] - M[n]) / (i - n) > right_max:
+                right_max = (M[i] - M[n]) / (i - n)
+                cnt += 1
+    return cnt
 
 
 ans = 0
-for m in M:
-    ans = check(m) if check(m) > ans else ans
+k = 0
+for i in range(len(M)):
+    ans = max(check(i), ans)
+print(ans)
