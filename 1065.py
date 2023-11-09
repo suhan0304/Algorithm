c80f9999b9679a64fc831f1f7146d2d43f973632
ZerrY <suhan0304@gmail.com>
2023-11-09 19:47:28 +0900

    1065

--- 1065.py
+++ 1065.py
@@ -4,7 +4,7 @@ x = int(sys.stdin.readline())
 
 
 def hansu(x):
-    if len(x) <= 99:
+    if x <= 99:
         return True
     else:
         if (x // 100 - (x // 10) % 10) == ((x // 10) % 10 - x % 10):
@@ -16,6 +16,5 @@ def hansu(x):
 ans = 0
 for i in range(1, x + 1):
     if hansu(i):
-        print(i)
         ans += 1
 print(ans)
