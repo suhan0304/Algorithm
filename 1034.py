464eff614905e559bf663c12d3e626b7dc7f544d
ZerrY <suhan0304@gmail.com>
2023-11-11 00:52:44 +0900

    1034

--- 1034.py
+++ 1034.py
@@ -4,13 +4,22 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-
-cnt = [0] * m
-ramp = []
+arr = []
 for _ in range(n):
-    ramp.append(input().strip())
-    for i in range(m):
-        if ramp[-1][i] == "0":
-            cnt[i] += 1
+    arr.append(input().strip())
+k = int(input())
 
-print(cnt)
+max_cnt = 0
+
+for i in range(n):
+    zero = 0
+    for n in arr[i]:
+        if n == "0":
+            zero += 1
+    cnt = 0
+    if zero <= k and zero % 2 == k % 2:
+        for j in range(n):
+            if arr[i] == arr[j]:
+                cnt += 1
+    max_cnt = max(max_cnt, cnt)
+print(max_cnt)
