ef8010295c0150b8a99a03c327548df418ef24f3
ZerrY <suhan0304@gmail.com>
2023-11-11 13:58:29 +0900

    1019

--- 1019.py
+++ 1019.py
@@ -1,6 +1,22 @@
 import sys
-import math
 
 input = sys.stdin.readline
 
-n = int(input())
+n = input().strip()
+ans = [0] * 10
+k = len(n)
+
+for x in n:
+    k -= 1
+    for i in range(int(x)):
+        ans[i] += 10**k
+        for j in range(10):
+            if k >= 1:
+                ans[j] += (10 ** (k - 1)) * k
+    ans[0] -= 10**k
+    if k:
+        ans[int(x)] += int("".join(n[-k:])) + 1
+    else:
+        ans[int(x)] += 1
+
+print(*ans)
