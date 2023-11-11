aeaa9905776865275ee538ff7a9656df690ffbee
ZerrY <suhan0304@gmail.com>
2023-11-12 02:45:02 +0900

    17413

--- 17413.py
+++ 17413.py
@@ -4,8 +4,30 @@ from collections import deque
 input = sys.stdin.readline
 
 s = input().strip()
-
-q = deque()
-bawards = False
+backward = True
 
 ans = []
+q = deque()
+
+i = 0
+
+while i < len(s):
+    if s[i] == "<":
+        while q:
+            ans.append(q.pop())
+        while s[i] != ">":
+            ans.append(s[i])
+            i += 1
+        ans.append(s[i])
+        i += 1
+    else:
+        if s[i] == " ":
+            while q:
+                ans.append(q.pop())
+            ans.append(s[i])
+        else:
+            q.append(s[i])
+        i += 1
+while q:
+    ans.append(q.pop())
+print("".join(ans))
