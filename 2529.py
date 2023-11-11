9990dcae46bc00f9a56a659131632684a9cf9cec
ZerrY <suhan0304@gmail.com>
2023-11-11 16:27:20 +0900

    2529

--- 2529.py
+++ 2529.py
@@ -2,8 +2,38 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
+K = int(input())
 
-arr = input().split()
+sign = list(input().split())
 
-print(arr)
+
+def check(i, j, k):
+    if k == "<":
+        return i < j
+    else:
+        return i > j
+
+
+def solve(depth, s):
+    global max_ans, min_ans
+
+    if depth == K + 1:
+        if len(min_ans) == 0:
+            min_ans = s
+        else:
+            max_anx = s
+
+    for i in range(10):
+        if not visited[i]:
+            if depth == 0 or check(s[-1], str(i), sign[depth - 1]):
+                visited[i] = True
+                solve(depth + 1, s + str(i))
+                visited[i] = False
+
+
+visited = [0] * 10
+min_ans = ""
+max_ans = ""
+solve(0, "")
+print(max_ans)
+print(min_ans)
