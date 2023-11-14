1600cf5d577ad7631e64b8bc9d860500982d8c4f
ZerrY <suhan0304@gmail.com>
2023-11-14 19:40:55 +0900

    1918

--- 1918.py
+++ 1918.py
@@ -1,46 +1,32 @@
 import sys
 from collections import deque
-from collections import Counter
 
 input = sys.stdin.readline
 
 s = input().strip()
 
 q = deque()
-oper = deque()
 
-cnt = 0
-
-q = deque()
-
-q.append('(')
-before_c = ''
-for c in s :
-    if c in ['+','-','*','/','(',')'] :
-        if c in ['*','/'] and before_c in ['+','-'] :
-            temp = q.pop()
-            q.append('(')
-            q.append(temp)
-        if c in ['+','-'] and before_c in ['*','/'] :
-            q.append(')')
-        before_c = c
-    q.append(c)
-
-for i in range(q.count('(') - q.count(')')) :
-    q.append(')')
-ans = deque()
-for c in q :
-    if c == ')' :
-        st = ""
-        while ans[-1] != '(' :
-            ch = ans.pop()
-            if ch in ['+','-','*','/'] :
-                st = st + ch
-            else :
-                st = ch + st 
-        ans.pop()
-        ans.append(st)
-    else :
+ans = []
+for c in s:
+    if c in ["+", "-", "*", "/", "(", ")"]:
+        if c == ")":
+            while q[-1] != "(":
+                ans.append(q.pop())
+            q.pop()
+        elif c in ["*", "/"]:
+            while q and q[-1] in ["*", "/"]:
+                ans.append(q.pop())
+            q.append(c)
+        elif c in ["+", "-"]:
+            while q and q[-1] in ["+", "-", "*", "/"]:
+                ans.append(q.pop())
+            q.append(c)
+        else:
+            q.append(c)
+    else:
         ans.append(c)
-print(ans.pop())
+while q:
+    ans.append(q.pop())
 
+print("".join(ans))
