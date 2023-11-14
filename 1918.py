3a7f784a6eaafc205d6ec79606e2634d63777c09
suhan0304 <suhan0304@gmail.com>
2023-11-14 17:07:51 +0900

    1918

--- 1918.py
+++ 1918.py
@@ -10,38 +10,21 @@ q = deque()
 oper = deque()
 
 cnt = 0
-"""
-for c in s:
+
+q = deque()
+before_c = ''
+for c in s :
     if c in ['+','-','*','/','(',')'] :
-        if c == '(' : 
-            cnt += 1
-        elif c== ')' :
-            cnt -= 1
-            
-        if c in ['*','/'] and oper and oper[-1] in ['+','-'] :
+        if c in ['*','/'] and before_c in ['+','-'] :
             temp = q.pop()
             q.append('(')
             q.append(temp)
-            oper.append('(')
-            cnt += 1
-        if c in ['+','-'] and oper and oper[-1] in ['*','/'] :
+        if c in ['+','-'] and before_c in ['*','/'] :
             q.append(')')
-            oper.append(')')
-            cnt -= 1
-            
-        oper.append(c)
+        before_c = c
     q.append(c)
 
-for i in range(cnt) :
-    oper.append(')')
+for i in range(q.count('(') - q.count(')')) :
     q.append(')')
-    
-if oper[0] != '(' :
-    oper.appendleft('(')
-    oper.append(')')
-    q.appendleft('(')
-    q.append(')')
-        
-print(q)
-"""
 
+print(q)
