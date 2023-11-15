2c8d743a986b0143845acc2139ea2d3ecfb25cb9
ZerrY <suhan0304@gmail.com>
2023-11-15 19:42:47 +0900

    10820

---
+++ 10820.py
@@ -0,0 +1,19 @@
+import sys
+
+while True:
+    try:
+        s = input()
+        ans = [0] * 4
+        for c in s:
+            if c.islower():
+                ans[0] += 1
+            elif c.isupper():
+                ans[1] += 1
+            elif c.isnumeric():
+                ans[2] += 1
+            elif c.isspace():
+                ans[3] += 1
+        print(" ".join(map(str, ans)))
+
+    except:
+        break
