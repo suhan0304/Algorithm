058478735d5b58a9967bc03083ce659390b20b4b
ZerrY <suhan0304@gmail.com>
2023-11-17 03:06:59 +0900

    11727

--- 11727.py
+++ 11727.py
@@ -1,6 +1,7 @@
 import sys
+
 n = int(sys.stdin.readline())
-ans = [1, 1]
-for i in range(2, n+1) :
-    ans.append(ans[i-1] + ans[i-2]*2)
-print(ans[n]%10007)
+ans = [0, 1, 3]
+for i in range(3, n + 1):
+    ans.append(ans[i - 1] + ans[i - 2] * 2)
+print(ans[n] % 10007)
