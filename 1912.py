5711f46b29ccbb79268f2f6f97c8ac097d7ab79c
ZerrY <suhan0304@gmail.com>
2023-11-17 03:14:16 +0900

    1912

---
+++ 1912.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+a = list(map(int, input().split()))
+
+ans = [0] * (n + 1)
+ans = [a[0]]
+
+for i in range(1, n):
+    ans.append(max(a[i] + ans[i - 1], a[i]))
+
+print(max(ans))
