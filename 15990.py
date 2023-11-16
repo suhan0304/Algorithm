bb6a29cc9f9dcbb43ec833eb937c00c67698337d
ZerrY <suhan0304@gmail.com>
2023-11-16 22:16:22 +0900

    15990

---
+++ 15990.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+idx = []
+for _ in range(n):
+    idx.append(int(sys.stdin.readline()))
+m = max(idx)
+
+ans = [1, 1, 2]
+
+for i in range(3, m + 1):
+    ans.append(sum(ans[i - 3 : i]))
+
+for i in idx:
+    print(ans[i])
