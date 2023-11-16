8430cfd01c372777024dcdf4f1e0b180f30b9b75
ZerrY <suhan0304@gmail.com>
2023-11-17 03:36:58 +0900

    1699

---
+++ 1699.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+ans = [i for i in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, int(i ** (0.5)) + 1):
+        ans[i] = min(ans[i - (j**2)] + 1, ans[i] + 1, ans[i])
+
+print(ans[64], ans[128], ans[753])
+print(ans[n])
