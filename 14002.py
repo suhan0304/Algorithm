33253085269c724012ba21e1a747fff9f7039504
ZerrY <suhan0304@gmail.com>
2023-11-17 02:06:49 +0900

    14002

--- 14002.py
+++ 14002.py
@@ -8,14 +8,21 @@ a = list(map(int, input().split()))
 
 ans = [[a[0]]]
 
+ans_max = 1
+ans_list = [a[0]]
+
 for i in range(1, n):
     max_len = 1
     max_list = [a[i]]
     for j in range(i - 1, -1, -1):
         if ans[j][-1] < a[i]:
             if len(ans[j]) + 1 > max_len:
-                max_len = len(ans[j]) + 1
                 max_list = ans[j] + [a[i]]
+                max_len = len(max_list)
+                if max_len > ans_max:
+                    ans_max = max_len
+                    ans_list = max_list
     ans.append(max_list)
 
-print(ans)
+print(ans_max)
+print(*ans_list)
