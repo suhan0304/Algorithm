70494fa112bfcfe3bbf1b44da76a6c26bded8684
ZerrY <suhan0304@gmail.com>
2023-11-16 22:57:16 +0900

    11053

--- 11053.py
+++ 11053.py
@@ -4,9 +4,16 @@ input = sys.stdin.readline
 
 n = int(input())
 
-a = list(map, int(input().split()))
+a = list(map(int, input().split()))
 
-ans = [1]
-max_a = a[0]
+ans = [[a[0]]]
 
-# DP 쓰는 문제 - 어떻게 구현?
+max_len = 1
+for i in range(1, n):
+    for j in range(len(ans)):
+        if ans[j][-1] < a[i]:
+            ans.append(ans[j] + [a[i]])
+            if len(ans[-1]) > max_len:
+                max_len = len(ans[-1])
+
+print(max_len)
