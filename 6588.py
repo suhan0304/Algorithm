c80fcbcf34966d25794c5bd070d9c3eb3bdd63ef
ZerrY <suhan0304@gmail.com>
2023-11-16 19:57:38 +0900

    6588

---
+++ 6588.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def eratosthenes_sieve(m):
+    isPrime = [False, False] + [True] * m
+    for i in range(2, int(m**0.5 + 1)):
+        if isPrime[i]:
+            for j in range(2 * i, m + 1, i):
+                isPrime[j] = False
+    prime = []
+    for i in range(m + 1):
+        if IsPrime[i]:
+            prime.append(i)
+
+    return prime
+
+
+IsPrime = [False, False] + [True] * 1000000
+prime = eratosthenes_sieve(1000000)
+
+print(1)
