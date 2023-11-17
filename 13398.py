77c2e959fdffcc73d23729ab2eaff28e0f67e3a4
ZerrY <suhan0304@gmail.com>
2023-11-18 03:09:25 +0900

    13398

--- 13398.py
+++ 13398.py
@@ -5,13 +5,11 @@ input = sys.stdin.readline
 n = int(input())
 a = list(map(int, input().split()))
 
-ans = [[a[0], a[0]]]
+dp = [[a[0], a[0]]]
 for i in range(1, n):
-    ans.append(
-        [max(ans[i - 1][0] + a[i], a[i]), max(ans[i - 1][0], ans[i - 1][1] + a[i])]
-    )
+    dp.append([max(dp[i - 1][0] + a[i], a[i]), max(dp[i - 1][0], dp[i - 1][1] + a[i])])
 
-max_ans = a[0]
+ans = a[0]
 for i in range(n):
-    max_ans = max(max_ans, max(ans[i][0], ans[i][1]))
-print(max_ans)
+    ans = max(ans, max(dp[i][0], dp[i][1]))
+print(ans)
