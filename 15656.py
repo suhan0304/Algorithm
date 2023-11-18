24a26935cec1b50b3a46d1c513c461a9d4a97d3d
ZerrY <suhan0304@gmail.com>
2023-11-19 04:56:41 +0900

    15656

---
+++ 15656.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+a = sorted(list(map(int, input().split())))
+
+
+def dfs(b, cnt):
+    if cnt == m:
+        print(*b)
+        return
+
+    for i in range(n):
+        dfs(b + [a[i]], cnt + 1)
+
+
+visited = [False] * n
+for i in range(n):
+    b = [a[i]]
+    dfs(b, 1)
