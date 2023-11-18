b990afb1a3b2ff8454ab4db3de9c66a964aa0779
ZerrY <suhan0304@gmail.com>
2023-11-18 16:46:03 +0900

    1476

--- 1476.py
+++ 1476.py
@@ -1,6 +1,8 @@
 import sys
 
-e, s, m = map(int, sys.stdin.readline().split())
+input = sys.stdin.readline
+
+e, s, m = map(int, input().split())
 
 ans = 1
 while not (ans % 15 == e % 15 and ans % 28 == s % 28 and ans % 19 == m % 19):
