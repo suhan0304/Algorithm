02ac8ce445d288d2766585bc16893bb58700ad4d
suhan0304 <suhan0304@gmail.com>
2023-11-21 17:41:39 +0900

    1248

--- 1248.py
+++ 1248.py
@@ -5,34 +5,30 @@ input = sys.stdin.readline
 n = int(input())
 
 sign = input().strip()
-
-def check_sign(a) :
-    sign_index = 0
-    dp = [[0 for _ in range(n)] for _ in range(n)]
-    for i in range(n) :
-        for j in range(i, n) :
-            if i == j :
-                dp[i][j] = a[i]
-            else :
-                dp[i][j] = dp[i][j-1] + a[j]
-            if not(dp[i][j] < 0  and sign[sign_index] == '-' or
-                   dp[i][j] == 0 and sign[sign_index] == '0' or 
-                   dp[i][j] > 0  and sign[sign_index] == '+') :
-                return False
-            sign_index += 1
-    else :
-        return True
-    
+s = [['' for _ in range(n)] for _ in range(n)]
+sign_index = 0
+for i in range(n) :
+    for j in range(i, n) :
+        s[i][j] = sign[sign_index]
+        sign_index += 1
+#print(s)
     
 def dfs(a, cnt) :
     if cnt == n :
-        if check_sign(a) :
-            print(*a)
-            exit()
+        print(*a)
         return
     
     for i in range(-10, 10+1, 1) :
+        dp[cnt][cnt] = i
+        for j in range(0, cnt) :
+            dp[j][cnt] = dp[j-1][cnt] + dp[cnt][cnt]
+            if not (dp[j][cnt] == 0 and s[j][cnt] == '0' or 
+                    dp[j][cnt] > 0 and s[j][cnt] == '+' or 
+                    dp[j][cnt] < 0 and s[j][cnt] == '-'):
+                return
         dfs(a + [i], cnt+1)
 
+dp = [[0 for _ in range(n)] for _ in range(n)]
 for i in range(-10, 10+1) :
+    dp[0][0] = i
     dfs([i], 1)
