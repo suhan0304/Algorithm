aa926a63c4151e0aa5b1d3e3fd54c44ae816be77
ZerrY <suhan0304@gmail.com>
2023-11-22 18:28:18 +0900

    2667

--- 2667.py
+++ 2667.py
@@ -3,8 +3,10 @@ import sys
 input = sys.stdin.readline
 
 
-def dfs(graph, i, j, num):
-    graph[i][j] = 0
+def dfs(graph, i, j):
+    global cnt
+    graph[i][j] = "0"
+    cnt += 1
 
     # 상 하 좌 후
     di = [-1, 1, 0, 0]
@@ -16,12 +18,30 @@ def dfs(graph, i, j, num):
         if ni < 0 or n <= ni or nj < 0 or n <= nj:
             continue
 
-        if graph[ni][nj] == 1:
+        if graph[ni][nj] == "1":
             dfs(graph, ni, nj)
 
 
 n = int(input())
 graph = []
+arr = []
 
 for _ in range(n):
     graph.append(list(input().strip()))
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == "1":
+            arr.append([i, j])
+
+ans = []
+for i, j in arr:
+    if graph[i][j] == "1":
+        cnt = 0
+        dfs(graph, i, j)
+        ans.append(cnt)
+
+ans.sort()
+print(len(ans))
+for a in ans:
+    print(a)
