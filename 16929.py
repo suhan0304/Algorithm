063ef2ba3bfdb6e507ec4ebf7383408f84650475
ZerrY <suhan0304@gmail.com>
2023-11-22 17:27:39 +0900

    16929

--- 16929.py
+++ 16929.py
@@ -4,7 +4,14 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-d = []
+# 상 하 좌 우
+dydx = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+def dfs(startX,startY)
+
+graph = []
 
 for _ in range(n):
-    d.append(input().strip())
+    graph.append(input().strip())
+
+
