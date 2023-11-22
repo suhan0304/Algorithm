c7dfc3bb5c23de22158c1c02776af7770f95366b
ZerrY <suhan0304@gmail.com>
2023-11-22 17:37:43 +0900

    13023

--- 13023.py
+++ 13023.py
@@ -2,25 +2,32 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
 
+def dfs(graph, v, visited, cnt):
+    if cnt == n:
+        global ans
 
-def dfs(graph, v, visited):
+    print(v)
+
     visited[v] = True
     for i in graph[v]:
         if not visited[i]:
-            dfs(graph, i, visited)
+            dfs(graph, i, visited, cnt + 1)
     return
 
-
+#input
+n, m = map(int, input().split())
+graph = [[] for _ in range(n)]
 for _ in range(m):
     u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
 
+ans = 0
+visited = [False] * n
 
-visited = [False] * (n + 1)
-for i in range(1, n + 1):
+for i in range(0, n):
     if not visited[i]:
-        dfs(graph, i, visited)
+        dfs(graph, i, visited, 1)
+
+print(ans)
