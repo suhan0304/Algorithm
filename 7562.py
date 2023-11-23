537716a80face49d16c7334d355dd0572ec66d27
ZerrY <suhan0304@gmail.com>
2023-11-24 00:41:12 +0900

    7562

--- 7562.py
+++ 7562.py
@@ -3,31 +3,37 @@ from collections import deque
 
 input = sys.stdin.readline
 
-q = deque()
-l = int(input())
-x1, y1 = map(int, input().split())
-x2, y2 = map(int, input().split())
+t = int(input())
 
-di = [-2, -2, 2, 2, -1, -1, 1, 1]
-dj = [-1, 1, -1, 1, -2, 2, -2, 2]
+for _ in range(t):
+    q = deque()
+    l = int(input())
 
-q.append([x1, y1, 0])
-visited = []
+    graph = [[False for _ in range(l)] for _ in range(l)]
 
-while q:
-    i, j, depth = q.popleft()
+    x1, y1 = map(int, input().split())
+    x2, y2 = map(int, input().split())
 
-    if i == x2 and j == y2:
-        print(depth)
-        exit()
+    di = [-2, -2, 2, 2, -1, -1, 1, 1]
+    dj = [-1, 1, -1, 1, -2, 2, -2, 2]
 
-    for d in range(8):
-        ni = i + di[d]
-        nj = j + dj[d]
+    q.append([x1, y1, 0])
+    visited = []
 
-        if ni < 0 or l <= ni or nj < 0 or l <= nj:
-            continue
+    while q:
+        i, j, depth = q.popleft()
 
-        if [ni, nj] not in visited:
-            q.append([nj, ni, depth + 1])
-            visited.append([ni, nj])
+        if i == x2 and j == y2:
+            print(depth)
+            break
+
+        for d in range(8):
+            ni = i + di[d]
+            nj = j + dj[d]
+
+            if ni < 0 or l <= ni or nj < 0 or l <= nj:
+                continue
+
+            if [ni, nj] not in visited:
+                q.append([nj, ni, depth + 1])
+                visited.append([ni, nj])
