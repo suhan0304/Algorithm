dafbac8681b79b6caba4d45b1f9f92a9739446c4
ZerrY <suhan0304@gmail.com>
2023-11-23 19:17:11 +0900

    2206

--- 2206.py
+++ 2206.py
@@ -4,31 +4,31 @@ import copy
 
 input = sys.stdin.readline
 
-def bfs() :
+
+def bfs():
     dist = [[[0 for _ in range(m)] for _ in range(n)] for _ in range(2)]
-    
+
     q = deque()
-    q.append([0, 0,0])
-    
-    di = [1,0,-1,0]
-    dj = [0,1,0,-1]
-    
-    graph[0][0][0],graph[1][0][0] = "-","-"
-        
-    while q :
+    q.append([0, 0, 0])
+
+    di = [1, 0, -1, 0]
+    dj = [0, 1, 0, -1]
+
+    graph[0][0][0], graph[1][0][0] = "-", "-"
+
+    while q:
         i, j, wall = q.popleft()
-        
-        if i == n-1 and j == m-1 :
+
+        if i == n - 1 and j == m - 1:
             print(dist[i][j][wall])
             exit
-            
 
-        
     return
 
-n,m = map(int,input().split())
+
+n, m = map(int, input().split())
 temp = []
-for _ in range(n) :
+for _ in range(n):
     temp.append(list(input().strip()))
 
 
