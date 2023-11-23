4be50071ed0b280e04684f5a353a43f328ca8ce2
suhan0304 <suhan0304@gmail.com>
2023-11-23 16:08:27 +0900

    2206

--- 2206.py
+++ 2206.py
@@ -1,20 +1,22 @@
 import sys
 from collections import deque
+import copy
 
 input = sys.stdin.readline
 
-def bfs() :
+def bfs(g) :
+    g_index = 0
     q = deque()
-    q.append([0,0,0])
+    q.append([0,0,0,0,0])
     
-    dist = [[0 for _ in range(m)] for _ in range(n) ]
-    graph[0][0] = "-"
-        
+    graph = []
+    graph.append(copy.deepcopy(g))
+    graph[0][0][0] = "-"
     while q :
-        i, j, wall = q.popleft()
+        i, j, wall, dist, g_index = q.popleft()
         
         if i == n-1 and j == m-1 :
-            print(dist[i][j]+1)
+            print(dist+1)
             exit()
             
         di = [1, 0, -1, 0]
@@ -27,23 +29,22 @@ def bfs() :
             if ni < 0 or n<= ni or nj < 0 or m <= nj :
                 continue
             
-            if graph[ni][nj] == '0' :
-                q.append([ni, nj, wall])
-                graph[ni][nj] = "-"
-                dist[ni][nj] = dist[i][j] + 1
-            
-            if graph[ni][nj] == '1' and wall == 0:
-                q. append([ni, nj, 1])
-                graph[ni][nj] = "-"
-                dist[ni][nj] = dist[i][j] + 1
+            if graph[g_index][ni][nj] == '0' :
+                graph[g_index][ni][nj] = "-"
+                q.append([ni, nj, wall, dist+1, g_index])
+                
+            if graph[g_index][ni][nj] == '1' and wall == 0:
+                graph.append(copy.deepcopy(graph[0]))
+                graph[g_index+1][ni][nj] = "-"
+                q.append([ni, nj, 1, dist+1, g_index+1])
         
     print(-1)
     
 
 n, m = map(int,input().split())
-graph = []
+g = []
 
 for _ in range(n) :
-    graph.append(list(input().strip()))
+    g.append(list(input().strip()))
 
-bfs()
+bfs(g)
