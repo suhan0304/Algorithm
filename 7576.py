4f35aaaeb634b112da4f14d0ac1d7a39f9ed12bb
ZerrY <suhan0304@gmail.com>
2023-11-23 23:02:11 +0900

    7576

--- 7576.py
+++ 7576.py
@@ -3,30 +3,30 @@ from collections import deque
 
 input = sys.stdin.readline
 
-m, n = map(int, input().split())
-
-graph = [list(map(int, input().split())) for _ in range(n)]
-
 
 def bfs(graph, i, j):
-    q_1 = deque()
-    q_1.append([0, 0, 1])
-
-    q_2 = deque()
-
     di = [1, 0, -1, 0]
     dj = [0, 1, 0, -1]
 
+    for i, j in arr:
+        q.append([i, j, 1])
 
-k = 0
+    while q:
+        i, j, depth = q.popleft()
+
+
+m, n = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+q = deque()
+
+tomato = 0
 arr = []
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 0:
-            k += 1
+            tomato += 1
         if graph[i][j] == 1:
             arr.append([i, j])
-
-print(arr)
 
 bfs()
