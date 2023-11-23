a9cf41b1bb2a1ed980e7b794c988b3b577cfa74c
ZerrY <suhan0304@gmail.com>
2023-11-24 02:04:20 +0900

    7562

--- 7562.py
+++ 7562.py
@@ -5,35 +5,33 @@ input = sys.stdin.readline
 
 t = int(input())
 
-for _ in range(t):
-    q = deque()
-    l = int(input())
 
-    graph = [[False for _ in range(l)] for _ in range(l)]
-
-    x1, y1 = map(int, input().split())
-    x2, y2 = map(int, input().split())
-
+def bfs():
     di = [-2, -2, 2, 2, -1, -1, 1, 1]
     dj = [-1, 1, -1, 1, -2, 2, -2, 2]
-
-    q.append([x1, y1, 0])
-    visited = [[False for _ in range(l)] for _ in range(l)]
+    q = deque()
+    q.append([x1, y1])
 
     while q:
-        i, j, depth = q.popleft()
-
+        i, j = q.popleft()
         if i == x2 and j == y2:
-            print(depth)
-            break
+            return graph[i][j] - 1
 
         for d in range(8):
             ni = i + di[d]
             nj = j + dj[d]
 
-            if ni < 0 or l <= ni or nj < 0 or l <= nj:
-                continue
+            if 0 <= ni < l and 0 <= nj < l and graph[ni][nj] == 0:
+                graph[ni][nj] = graph[i][j] + 1
+                q.append([nj, ni])
 
-            if not visited[ni][nj]:
-                visited[ni][nj] = True
-                q.append([nj, ni, depth + 1])
+
+for _ in range(t):
+    l = int(input())
+
+    x1, y1 = map(int, input().split())
+    x2, y2 = map(int, input().split())
+
+    graph = [[0] * l for _ in range(l)]
+    graph[x1][y1] = 1
+    print(bfs())
