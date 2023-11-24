0b4e295376c4614412d18a6c36ca244860bbde7b
ZerrY <suhan0304@gmail.com>
2023-11-24 15:26:29 +0900

    16947

--- 16947.py
+++ 16947.py
@@ -9,7 +9,7 @@ input = sys.stdin.readline
 find_route = False
 
 
-def findRing(graph, route, v, visited):
+def findRing(route, v, visited):
     global find_route
     if find_route:
         return
@@ -17,16 +17,17 @@ def findRing(graph, route, v, visited):
     for i in graph[v]:
         if not visited[i]:
             dist[i] = dist[v] + 1
-            findRing(graph, route + [i], i, visited)
+            findRing(route + [i], i, visited)
         if visited[i] and dist[v] >= dist[i] + 2:
             find_route = True
-            print(i, v, route, dist[v], dist[i], dist)
+            # print(i, v, route, dist[v], dist[i], dist)
             for j in route[dist[i] : dist[v] + 1]:
                 ring[j] = -1
             return route
 
 
-def bfs(graph, start):
+# bfs
+def bfs(start):
     q = deque()
     q.append([start, 0])
 
@@ -41,14 +42,6 @@ def bfs(graph, start):
             if not visited[i]:
                 visited[i] = True
                 q.append([i, depth + 1])
-
-    if ring[v] == -1:
-        return depth
-
-    for i in graph[v]:
-        if not visited[i]:
-            visited[i] = True
-            dfs(graph, i, visited, depth + 1)
 
 
 # input
@@ -66,9 +59,9 @@ dist[0] = 0
 
 visited = [False] * n
 ring = [0 for _ in range(n)]
-findRing(graph, [0], 0, visited)
-print(ring)
+findRing([0], 0, visited)
+# print(ring)
 
 # solve
 for i in range(n):
-    print(bfs(graph, i), end=" ")
+    print(bfs(i), end=" ")
