ba0529641a3b4c33acbe6de38dd675ef870f65a0
ZerrY <suhan0304@gmail.com>
2023-11-24 14:48:59 +0900

    16947

--- 16947.py
+++ 16947.py
@@ -4,27 +4,42 @@ sys.setrecursionlimit(10**6)
 
 input = sys.stdin.readline
 
-
-def dfs(graph, v, visited):
-    visited[v] = True
-    for i in graph[v]:
-        if not visited[i]:
-            dist[i] = dist[v] + 1
-            dfs(graph, i, visited)
-        if visited[i] and dist[v] >= dist[i] + 2:
-            print("Yes")
+# 하 우 상 좌
+di = [1, 0, -1, 0]
+dj = [0, 1, 0, -1]
 
 
-n = int(input().rstrip())
-graph = [[0] * n for _ in range(n)]
+def dfs(i, j, start):
+    for d in range(4):
+        ni = i + di[d]
+        nj = j + dj[d]
+
+        if 0 <= ni < n and 0 <= nj < n and graph[ni][nj] == start:
+            if not visited[ni][nj]:
+                visited[ni][nj] = True
+                dist[ni][nj] = dist[i][j] + 1
+                dfs(ni, nj, start)
+            elif visited[ni][nj] and dist[ni][nj] >= dist[i][j] + 3:
+                print("Yes")
+                exit()
+
+
+graph = [[0] * n]
+
 for _ in range(n):
-    u, v = map(int, input().rstrip().split())
-    u, v = u - 1, v - 1
-    graph[u].append(v)
-    graph[v].append(u)
+    graph.append(input().strip())
 
-dist = [-1] * n
-dist[0] = 1
-visited = [False] * n
-dfs(graph, 0, visited)
-print(dist)
+dist = [[0 for _ in range(n)] for _ in range(n)]
+
+"""
+visited = [[False] * n for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            dist[i][j] = 1
+            visited[i][j] = True
+            dfs(i, j, graph[i][j])
+
+            
+print("No")
+"""
