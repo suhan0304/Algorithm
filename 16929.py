7049d4fd96da2ab12cfb50a0081152b8ba92e7fc
ZerrY <suhan0304@gmail.com>
2023-11-24 14:01:26 +0900

    16929

--- 16929.py
+++ 16929.py
@@ -4,14 +4,39 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-# 상 하 좌 우
-dydx = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+# 하 우 상 좌
+di = [1, 0, -1, 0]
+dj = [0, 1, 0, -1]
 
-def dfs(startX,startY)
+
+def dfs(i, j, start):
+    for d in range(4):
+        ni = i + di[d]
+        nj = j + dj[d]
+
+        if 0 <= ni < n and 0 <= nj < m and graph[ni][nj] == start:
+            if not visited[ni][nj]:
+                visited[ni][nj] = True
+                dist[ni][nj] = dist[i][j] + 1
+                dfs(ni, nj, start)
+            elif visited[ni][nj] and dist[ni][nj] >= dist[i][j] + 3:
+                print(dist)
+                print("Yes")
+                exit()
+
 
 graph = []
 
 for _ in range(n):
     graph.append(input().strip())
 
+dist = [[0 for _ in range(m)] for _ in range(n)]
 
+visited = [[False] * m for _ in range(n)]
+for i in range(n):
+    for j in range(m):
+        if not visited[i][j]:
+            visited[i][j] = True
+            dfs(i, j, graph[i][j])
+
+print("No")
