a14d938e2bf1d88b7f7800741901c9328f301df9
ZerrY <suhan0304@gmail.com>
2023-11-24 14:46:32 +0900

    16947

--- 16947.py
+++ 16947.py
@@ -5,25 +5,26 @@ sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 
-def dfs(graph, v, visited, d):
+def dfs(graph, v, visited):
     visited[v] = True
     for i in graph[v]:
         if not visited[i]:
-            dist[v][i] = d
-            dfs(graph, i, visited, d + 1)
+            dist[i] = dist[v] + 1
+            dfs(graph, i, visited)
+        if visited[i] and dist[v] >= dist[i] + 2:
+            print("Yes")
 
 
 n = int(input().rstrip())
-graph = [[] for _ in range(n)]
+graph = [[0] * n for _ in range(n)]
 for _ in range(n):
     u, v = map(int, input().rstrip().split())
     u, v = u - 1, v - 1
     graph[u].append(v)
     graph[v].append(u)
 
-dist = [[0] * n for _ in range(n)]
+dist = [-1] * n
+dist[0] = 1
 visited = [False] * n
-dfs(graph, 1, visited, 1)
-
-for d in dist:
-    print(d)
+dfs(graph, 0, visited)
+print(dist)
