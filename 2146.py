62cb4692118b65f8279def1cf0e11c6ff9d46618
ZerrY <suhan0304@gmail.com>
2023-11-25 15:45:35 +0900

    2146

--- 2146.py
+++ 2146.py
@@ -12,3 +12,33 @@ dj = [0, 1, 0, -1]
 visited = [[False] * n for _ in range(n)]
 
 q = deque()
+
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            visited[i][j] = True
+            q.append([i, j])
+            island_num = 2
+            while q:
+                i, j = q.popleft()
+                graph[i][j] = island_num
+
+                for d in range(4):
+                    ni = i + di[d]
+                    nj = j + dj[d]
+
+                    if 0 <= ni < n and 0 <= nj < n and graph[ni][nj] == 1:
+                        visited[ni][nj] = True
+                        graph[ni][nj] = island_num
+                        q.append([ni, nj])
+
+            island_num += 1
+
+for g in graph:
+    print(g)
+
+
+print()
+for g in graph:
+    print(g)
