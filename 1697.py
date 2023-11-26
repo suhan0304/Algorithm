4ebf56f05aee40cc455e68d71140750fbf6ca4fe
ZerrY <suhan0304@gmail.com>
2023-11-26 21:37:01 +0900

    1697

--- 1697.py
+++ 1697.py
@@ -7,6 +7,10 @@ M = 10 ** 5
 
 n, k = map(int, input().rstrip().split())
 
+if n == k :
+    print(0)
+    exit()
+
 visited = [False] * (M+1)
 q = deque()
 
@@ -16,11 +20,10 @@ while q :
     m, depth = q.popleft()
 
     for i in [m-1, m+1, 2*m] :
-        if i < 0 or 200000 < i : continue
+        if i < 0 or 100000 < i : continue
         if visited[i] : continue
-        if i+1 == k :
-            print(depth)
+        if i == k :
+            print(depth+1)
             exit()
-
+        visited[i] = True
         q.append([i, depth+1])
-    
