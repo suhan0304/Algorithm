80d743b360acca6d4cc7aa8fc6632d8d7926931b
ZerrY <suhan0304@gmail.com>
2023-11-29 02:06:43 +0900

    14225

---
+++ 14225.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+arr = list(map(int,input().rstrip().split()))
+
+ans = [0 for _ in range(sum(arr)+2)] 
+
+def dfs(b, idx) :
+    ans[sum(b)] = -1
+
+    for i in range(idx, n) :
+        b.append(arr[i])
+        dfs(b, i+1) 
+        b.pop()
+
+dfs([], 0)
+
+print(ans)
+
+for i in range(len(ans)) :
+    if ans[i] == 0 :
+        print(i)
+        break
