fcd230fda1e9c826638b4b8c7fbdbc124816839e
ZerrY <suhan0304@gmail.com>
2023-11-29 01:29:30 +0900

    1339

--- 1339.py
+++ 1339.py
@@ -1,11 +1,22 @@
 import sys
-
+from collections import deque
 input = sys.stdin.readline
 
 n = int(input().rstrip())
-max_length = 0
-string = []
-for _ in range(n) :
-    string = [list(input().rstrip())]
+strings = [input().rstrip() for _ in range(n)]
+max_length = max(len(s) for s in strings)
 
+padded_strings = [s.rjust(max_length) for s in strings]
 
+cnt = [0 for _ in range(10)] 
+for i in range(max_length) :
+    for s in padded_strings :
+        if s[i] != ' ' :
+            cnt[ord(s[i])-ord('A')] += (10**(max_length - i-1))
+
+cnt.sort(reverse=True)
+
+ans = 0
+for i in range(10) :
+    ans += cnt[i] * (9-i)
+print(ans)
