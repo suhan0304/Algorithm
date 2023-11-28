cec600566fba6f1cd469f036f9731ea9e6d4bb77
suhan0304 <suhan0304@gmail.com>
2023-11-28 16:55:01 +0900

    1520

--- 1520.py
+++ 1520.py
@@ -1,16 +1,9 @@
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**8)
-# input
-m, n = map(int, input().rstrip().split())
-graph = [list(map(int,input().rstrip().split())) for _ in range(m)]
-dp = [[-1] * n for _ in range(m)]
-dp[0][0] = 1
 
 di = [1, 0, -1, 0]
 dj = [0, 1, 0, -1]
-
-ans = 0
 
 # dfs
 def dfs(i, j) :
@@ -20,18 +13,21 @@ def dfs(i, j) :
     if dp[i][j] != -1 :
         return dp[i][j]
         
-    dp[i][j] = 0
+    cnt = 0
     
     for d in range(4) :
         ni = i + di[d]
         nj = j + dj[d]
         
         if 0 <= ni < m and 0 <= nj < n and graph[ni][nj] < graph[i][j] :
-            dp[i][j] += dfs(ni,nj)
+            cnt += dfs(ni, nj)
     
+    dp[i][j] = cnt
     return dp[i][j]
     
 
+# input
+m, n = map(int, input().rstrip().split())
+graph = [list(map(int,input().rstrip().split())) for _ in range(m)]
+dp = [[-1] * n for _ in range(m)]
 print(dfs(0, 0))
-for d in dp :
-    print(d)
