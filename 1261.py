751050d15b8737285c94189d94ff19296b88aa0a
suhan0304 <suhan0304@gmail.com>
2023-11-28 17:03:13 +0900

    1261

---
+++ 1261.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+# input
+m, n = map(int,input().rstrip().split())
+graph = [list(input().rstrip())]
+visited = [[False] * m for _ in range(n)]
+
+# bfs
+def bfs() :
+    di = [1, 0, -1, 0]
+    dj = [0, 1, 0, -1]
+    
+    q = deque()
+    q.append([0, 0, 0])
+    while q :
+        i, j, wall = q.popleft()
+
+        if i == n-1 and j == m-2 :
+            print(wall)
+            break
+        
+        for d in range(4) :
+            ni = i + di[d]
+            nj = j + dj[d]
+            
+            if 0 <= ni < n and 0 <= nj < m and not visited[ni][nj] :
+                visited[ni][nj]
+                if graph[ni][nj] == '0' :
+                    q.appendleft([ni,nj,wall])
+                if graph[ni][nj] == '1' :
+                    q.append([ni,nj,wall+1])
