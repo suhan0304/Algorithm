caa93740df03f5f09e275ba07dddd89dcf36d6ef
ZerrY <suhan0304@gmail.com>
2023-11-30 22:56:36 +0900

    16198

--- 16198.py
+++ 16198.py
@@ -1,18 +1,22 @@
 import sys
-
+sys.setrecursionlimit(10**8)
 input = sys.stdin.readline
 
 n = int(input())
 
-arr = list(map,int(input().rstrip().split()))
+arr = list(map(int, input().rstrip().split()))
 
 ans = 0
 
 def proc(a, dsum, cnt) :
     if cnt == len(arr) - 2:
+        global ans
         ans = max(ans,dsum)
+        return 
 
-    for i in range(1,len(arr)-1) :
-        score = 
-        temp = a.pop(i)
+    for i in range(1,len(a)-1) :
+        proc(a[0:i] + a[i+1:len(a)], dsum + a[i-1] * a[i+1], cnt + 1)
+
+proc(arr, 0, 0)
+print(ans)
 
