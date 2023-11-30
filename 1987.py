5d37c34e931c280f71ac07a351b731d78dc2b84e
ZerrY <suhan0304@gmail.com>
2023-11-30 23:55:47 +0900

    1987

--- 1987.py
+++ 1987.py
@@ -10,21 +10,23 @@ dj = [0,1,0,-1]
 
 visited = [False]  * 26
 
-print(visited)
-
 max_ans = 0
 
 def dfs(i, j, visited, depth) :
     global max_ans
-    max_ans = max(depth,max_ans)
+    if depth > max_ans :
+        print(depth, [chr(i+ord('A')) for i in range(26) if visited[i]])
+        max_ans = depth
 
     for d in range(4) :
         ni = i + di[d]
         nj = j + dj[d]
 
-        if 0 <= ni < r and 0 <= nj < c and not visited[ord(graph[ni][nj])-ord['A']] :
-            visited[ord(graph[ni][nj])] = True
+        if 0 <= ni < r and 0 <= nj < c and not visited[ord(graph[ni][nj])-ord('A')] :
+            visited[ord(graph[ni][nj])-ord('A')] = True
             dfs(ni,nj,visited, depth + 1)
-            visited[ord(graph[ni][nj])] = False
+            visited[ord(graph[ni][nj])-ord('A')] = False
 
-dfs(0, 0, visited, 0)
+visited[ord(graph[0][0])-ord('A')] = True
+dfs(0, 0, visited, 1)
+print(max_ans)
