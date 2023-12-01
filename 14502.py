59e9ee9be7eb2042a5ec8b59594ced919368ef01
ZerrY <suhan0304@gmail.com>
2023-12-01 19:20:12 +0900

    14502

--- 14502.py
+++ 14502.py
@@ -1,12 +1,18 @@
 import sys
 from collections import deque
+from itertools import combinations
 input = sys.stdin.readline
 
 # input
 n, m = map(int, input().rstrip().split())
 graph = [list(map(int, input().rstrip().split())) for _ in range(n)]
-virus = [(i, j) for i in range(n) for j in range(m) if graph[i][j] == 2]
 
+empty = []
+virus = []
+for i in range(n) :
+    for j in range(m) :
+        if graph[i][j] == 0 : empty.append((i,j))
+        if graph[i][j] == 2 : virus.append((i,j))
             
 di = [1, 0, -1, 0]
 dj = [0, 1, 0, -1]
@@ -30,10 +36,26 @@ def bfs(graph) :
 #solve
 def solution() :
     max_ans = 0
+    max_graph = []
+    walls_comb = list(combinations(empty, 3))
 
+    for walls in walls_comb :
+        for wall in walls :
+            graph[wall[0]][wall[1]] = 1
     
+        bfs(graph)
 
-bfs()
-for g in graph :
-    print(g)
+        ans = count_safe_area(graph)
+        if max_ans < ans :
+            max_ans = ans
+            max_graph = graph.copy()
+            
+        for wall in walls :
+            graph[wall[0]][wall[1]] = 0
+
+    for g in max_graph :
+        print(g)
+    return max_ans
+
+print(solution())
 
