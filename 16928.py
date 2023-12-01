fa998af130775e93a10ac75688cd9bf8ad53b793
ZerrY <suhan0304@gmail.com>
2023-12-01 18:37:36 +0900

    16928

--- 16928.py
+++ 16928.py
@@ -1,12 +1,31 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
 n, m = map(int, input().rstrip().split())
 graph = [i for i in range(101)]
-visited = [0 for _ in range(101)]
 
 for _ in range(n + m) :
     u, v = map(int, input().rstrip().split())
     graph[u] = v
+
+
+def bfs() :
+    visited = [False for _ in range(101)]
+    q = deque()
+    q.append([1, 0])
+    visited[1] = True
+    while q :
+        i, depth = q.popleft()
+
+        if i == 100 :
+            print(depth)
+            exit()
+
+        for d in range(1,7) :
+            ni = i + d
+
+            if 0 <= ni < 101 and not graph[ni] :
+                q.append([graph[ni], depth+1])
 
