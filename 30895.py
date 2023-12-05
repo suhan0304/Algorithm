1b5ee68fabcaa2e94becc8d0aa2449295743e728
ZerrY <102403989+suhan0304@users.noreply.github.com>
2023-12-05 17:08:58 +0900

    30895

--- 30895.py
+++ 30895.py
@@ -3,41 +3,25 @@ import math
 
 input = sys.stdin.readline
 
-def find_eff(X, Y, m) :
-    return math.ceil(m//X) / math.ceil(m//(X+Y))
-
-def find_range(X, Y, K) :
-    m = math.lcm(X, X+Y)
-    n = m
-    while n <= K :
-        n += m
-    return n-m+1, n+m+1
-
 def solution(X, Y, K) :
-    start, end = find_range(X, Y, K)
-    #print(start, end)
+    ran = math.lcm(X, X+Y)
     
-    min_eff = math.ceil(K//X) / math.ceil(K//(X+Y))
+    min_eff = (math.ceil(K/X) / math.ceil(K/(X+Y)))
     ans = K
-    
-    for i in range(start, end, X) :
-        print(i, eff, end=' ||')
-        if i <= K :
-            continue
-        eff = find_eff(X, Y, i)
-        if eff < min_eff :
-            min_eff = eff
-            ans = i
-    for i in range(start, end, X+Y) :
-        print(i, eff, end=' ||')
-        if i <= K :
-            continue
-        eff = find_eff(X, Y, i)
-        if eff < min_eff :
-            min_eff = eff
-            ans = i
-    print()
+    for j in range(1, 3) :
+        for i in range(K+1, K+j*ran+1, X) :
+            eff = (math.ceil(i/X) / math.ceil(i/(X+Y)))
+            if eff < min_eff :
+                min_eff = eff
+                ans = i
+            
         
+        for i in range(K+1, K+j*ran+1, X+Y) :
+            eff = (math.ceil(i/X) / math.ceil(i/(X+Y)))
+            if eff < min_eff :
+                min_eff = eff
+                ans = i
+    
     return ans
 
 X, Y, K = map(int, input().rstrip().split())
