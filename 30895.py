70ac20ef3feed4272f5722b521bc0fefd7644020
ZerrY <102403989+suhan0304@users.noreply.github.com>
2023-12-05 16:38:50 +0900

    30895

--- 30895.py
+++ 30895.py
@@ -11,26 +11,32 @@ def find_range(X, Y, K) :
     n = m
     while n <= K :
         n += m
-    return n, m
+    return n-m+1, n+m+1
 
 def solution(X, Y, K) :
-    n, m = find_range(X, Y, K)
+    start, end = find_range(X, Y, K)
+    #print(start, end)
     
     min_eff = math.ceil(K//X) / math.ceil(K//(X+Y))
     ans = K
     
-    for i in range(n-m+1, n+1, X) :
+    for i in range(start, end, X) :
+        print(i, eff, end=' ||')
+        if i <= K :
+            continue
         eff = find_eff(X, Y, i)
         if eff < min_eff :
             min_eff = eff
             ans = i
-        
-    for i in range(n-m+1, n+1, X+Y) :
+    for i in range(start, end, X+Y) :
+        print(i, eff, end=' ||')
+        if i <= K :
+            continue
         eff = find_eff(X, Y, i)
         if eff < min_eff :
             min_eff = eff
             ans = i
-            
+    print()
         
     return ans
 
