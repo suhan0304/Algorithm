2989149c5c1c16feef46f4d3abd5fc54dceddef0
ZerrY <102403989+suhan0304@users.noreply.github.com>
2023-12-05 16:16:48 +0900

    30895

--- 30895.py
+++ 30895.py
@@ -3,19 +3,39 @@ import math
 
 input = sys.stdin.readline
 
-X, Y, K = map(int, input().rstrip().split())
+def find_eff(X, Y, m) :
+    return math.ceil(m//X) / math.ceil(m//(X+Y))
 
+def find_range(X, Y, K) :
+    m = math.lcm(X, X+Y)
+    n = m
+    while n <= K :
+        n += m
+    return n, m
 
-
-def solution() :
-    eff = (math.ceil(K/X) / math.ceil(K/(X+Y)))
+def solution(X, Y, K) :
+    n, m = find_range(X, Y, K)
+    
+    min_eff = math.ceil(K//X) / math.ceil(K//(X+Y))
     ans = K
-    for i in range(K+1, K+math.lcm(X, X+Y)*2+1, X) :
-        cnt_none = math.ceil(i/X)
-        cnt_item = math.ceil(i/(X+Y))
-        if cnt_none/cnt_item < eff :
-            eff = cnt_none/cnt_item
+    
+    for i in range(n-m+1, n+1, X) :
+        eff = find_eff(X, Y, i)
+        if eff < min_eff :
+            min_eff = eff
             ans = i
+        
+    for i in range(n-m+1, n+1, X+Y) :
+        eff = find_eff(X, Y, i)
+        if eff < min_eff :
+            min_eff = eff
+            ans = i
+            
+        
+    return ans
 
-#print(eff)
-print(solution())
+X, Y, K = map(int, input().rstrip().split())
+print(solution(X,Y,K))
+
+
+    
