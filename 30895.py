ad5002555286f73f3b7b4127a2c2e3b63b177925
ZerrY <102403989+suhan0304@users.noreply.github.com>
2023-12-05 15:28:20 +0900

    30895

--- 30895.py
+++ 30895.py
@@ -5,12 +5,24 @@ input = sys.stdin.readline
 
 X, Y, K = map(int, input().rstrip().split())
 
-m = math.lcm(X, X+Y)
-print(m)
+def find_efficiency(m) :
+    return (math.ceil(m/X) / math.ceil(m/(X+Y)))
 
-ans = [0, ((K+1)//X)/(K+1)]
-for i in range(K+1, K+m+1) :
-    cnt_none = i//X
-    cnt_item = i//(X+Y)
+def solution(X,Y, K) :
+    lowest_ans = 10**9
+    left, right = K, 10**9
     
-    if cnt_none/cnt_item < ans[1] :
+    while left < right : 
+        mid = (left + right) // 2 
+        
+        x_eff = (mid + X - 1) // X
+        xy_eff = (mid + X + Y - 1) // (X + Y)
+        
+        if x_eff > xy_eff :
+            right = mid
+        else :
+            left = mid + 1
+    return left
+
+result = solution(X, Y, K)
+print("가장 낮은 효율의 정수:", result)
