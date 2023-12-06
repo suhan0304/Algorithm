59826f74623b6a4594c020e294064915df169dc2
ZerrY <suhan0304@gmail.com>
2023-12-06 21:18:55 +0900

    30892

--- 30892.py
+++ 30892.py
@@ -5,12 +5,3 @@ input = sys.stdin.readline
 N, K, T = map(int,input().rstrip().split())
 
 arr = sorted(list(map(int,input().rstrip().split())))
-
-cnt = 1
-q=deque(arr)
-size = T
-
-while q[0] < size :
-    eat = q.popleft()
-size += eat
-cnt += 1
