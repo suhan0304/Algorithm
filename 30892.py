1f6e5ebaa16ac04ff31bac64c4d53123e53286fe
ZerrY <suhan0304@gmail.com>
2023-12-06 21:22:34 +0900

    30892

--- 30892.py
+++ 30892.py
@@ -5,3 +5,15 @@ input = sys.stdin.readline
 N, K, T = map(int,input().rstrip().split())
 
 arr = sorted(list(map(int,input().rstrip().split())))
+
+sharks = deque(arr)
+can_eat = deque()
+
+for i in range(K) :
+    while sharks and sharks[0] < T :
+        can_eat.append(sharks.popleft())
+    if not can_eat :
+        break
+    T += can_eat.pop()
+
+print(T)
