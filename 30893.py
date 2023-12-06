89c1b567a6d9f1519b975acb3185a9a72941774a
ZerrY <suhan0304@gmail.com>
2023-12-06 22:10:23 +0900

    30893

--- 30893.py
+++ 30893.py
@@ -1,10 +1,32 @@
 import sys
+sys.setrecursionlimit(10**9)
 
+#input
 input = sys.stdin.readline
-
 N, S, E = map(int, input().rstrip().split())
-
+S, E = S-1, E-1
 graph = [[] for _ in range(N)]
-
-for _ in range(N) :
+for _ in range(N-1) :
     s, v = map(int, input().rstrip().split())
+    s, v = s-1, v - 1
+    graph[s].append(v)
+    graph[v].append(s)
+
+#dfs
+def dfs(v, player, visited) :
+    if v == E :
+        print("First" if player == 1 else "Second")
+        exit(0)
+    
+    for u in graph[v] :
+        if not visited[u] :
+            visited[u] = True
+            dfs(u, 2 if player == 1 else 1, visited)
+            visited[u] = False
+
+    return
+
+visited = [False for _ in range(N)]
+visited[S] = True
+dfs(S, 2, visited)
+ 
