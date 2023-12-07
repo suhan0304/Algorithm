cbca1c3089c876ff060f65fe36d2301d4fc7f9f6
ZerrY <suhan0304@gmail.com>
2023-12-07 18:51:30 +0900

    11074

--- 11074.py
+++ 11074.py
@@ -5,9 +5,17 @@ input = sys.stdin.readline
 N, K = map(int, input().rstrip().split())
 
 arr = [int(input().rstrip()) for _ in range(N)]
+arr.sort(reverse=True)
 
 def solution(K) :
     ans = 0
-    return ans
+    for i in range(0, len(arr)) :
+        if K > arr[i] :
+            while K > arr[i] :
+                K -= arr[i]
+                ans += 1
+        if K == 0 :
+            return ans
+    return 
 
-print(arr)
+print(solution(K))
