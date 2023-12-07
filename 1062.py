5a5bade30a72548d9c85a4aa6cc8f5deaeea188c
suhan0304 <suhan0304@gamil.com>
2023-12-07 12:40:11 +0900

    1062

--- 1062.py
+++ 1062.py
@@ -1,48 +1,29 @@
 import sys
+from itertools import combinations
 
 input = sys.stdin.readline
 
-N, K = map(int, input().rstrip().split())
+def word2bit(word):
+    bit = 0
+    for char in word:
+        bit = bit | (1 << ord(char) - ord('a'))
+    return bit
 
-arr = list(input().rstrip() for _ in range(N))
+N, K = map(int, input().split())
+words = [input().rstrip() for _ in range(N)]
+bits = list(map(word2bit, words))
+base_bit = word2bit('antic')
 
-def check(visited) :
-    cnt = 0
-    for s in arr :
-        for c in s :
-            if visited[char(c)] == 0 :
-                break
-        else :
-            cnt += 1
-    return cnt
-
-max_cnt = 0
-def bk_tr(depth, visited) :
-    if depth == K :
-        global max_cnt
-        max_cnt = max(check(visited), max_cnt)
-        return
-    
-    for s in arr :
-        for c in s :
-            if visited[char(c)] == 0 :
-                visited[char(c)] = 1
-                bk_tr(depth+1, visited)
-                visited[char(c)] = 0
-    return
-                
-def char(c) :
-    return ord(c) - ord('a')
-
-visited = [0] * 26
-if K < 5 :
+if K < 5:
     print(0)
-    exit(0)
-else :
-    visited[char('a')] = 1
-    visited[char('n')] = 1
-    visited[char('t')] = 1
-    visited[char('i')] = 1
-    visited[char('c')] = 1
-    bk_tr(5, visited)
-    print(max_cnt)
+else:
+    alphabet = [1 << i for i in range(26) if not (base_bit & 1 << i)]
+    answer = 0
+    for combination in combinations(alphabet, K-5):
+        know_bit = sum(combination) | base_bit
+        count = 0
+        for bit in bits:
+            if bit & know_bit == bit:
+                count += 1
+        answer = max(answer, count)
+    print(answer)
