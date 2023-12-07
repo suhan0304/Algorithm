cc77e45f527a0d35332a9acf80bb5b5e65f6852a
ZerrY <suhan0304@gmail.com>
2023-12-07 19:14:11 +0900

    1744

--- 1744.py
+++ 1744.py
@@ -1,5 +1,5 @@
 import sys
-
+from collections import deque
 input = sys.stdin.readline
 
 N = int(input())
@@ -11,3 +11,17 @@ for _ in range(N) :
         pos.append(k)
     else :
         neg.append(k)
+
+pos.sort()
+neg.sort()
+
+pos = deque(pos)
+neg = deque(neg)
+
+ans = 0
+while len(pos) >= 2 :
+    a, b= pos.pop() * pos.pop()
+    if a == 1 or b == 1 or a == 0 or b == 0:
+        ans += a + b
+    else :
+        ans += a * b
