bac8e9218233707078a167abb7b4cd9541d49f4a
ZerrY <suhan0304@gmail.com>
2023-12-07 18:52:26 +0900

    11074

--- 11074.py
+++ 11074.py
@@ -11,7 +11,7 @@ def solution(K) :
     ans = 0
     for i in range(0, len(arr)) :
         if K > arr[i] :
-            while K > arr[i] :
+            while K >= arr[i] :
                 K -= arr[i]
                 ans += 1
         if K == 0 :
