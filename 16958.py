d796c32f1a546f9ddaf634bf16f76c1604bd22c6
suhan0304 <suhan0304@gamil.com>
2023-12-07 12:48:52 +0900

    16958

--- 16958.py
+++ 16958.py
@@ -11,19 +11,23 @@ dc = [-1, 1, -2, 2, -1, 1]
 visited = [[-1 for _ in range(N)] for _ in range(N)]
 
 def bfs() :
-    q = deque
+    q = deque()
     q.append((r1, c1))
     visited[r1][c1] = 0
     
     while q :
         r, c = q.popleft()
+        
         if r==r2 and c==c2 :
             print(visited[r][c])
             exit(0)
         
         for d in range(6) :
             nr = r + dr[d]
-            nc = c + dc[c]
-            if visited[nr][nc] == -1 :
+            nc = c + dc[d]
+            if 0<=nr<N and 0<=nc<N and visited[nr][nc] == -1 :
                 visited[nr][nc] = visited[r][c] + 1
                 q.append((nr, nc))
+                
+bfs()
+print(-1)
