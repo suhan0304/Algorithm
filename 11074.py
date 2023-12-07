092cd711c2d33a32a1ede6be3b26031d4835cd9b
ZerrY <suhan0304@gmail.com>
2023-12-07 18:54:54 +0900

    11074

--- 11074.py
+++ 11074.py
@@ -4,19 +4,19 @@ input = sys.stdin.readline
 
 N, K = map(int, input().rstrip().split())
 
-coin = []
+coins = []
 
 for i in range(N) :
-    coin.append(int(input()))
+    coins.append(int(input()))
+
 def solution(K) :
     ans = 0
-    for i in range(0, len(arr)) :
-        if K > arr[i] :
-            while K >= arr[i] :
-                K -= arr[i]
-                ans += 1
-        if K == 0 :
-            return ans
-    return 
+    for coin in coins :
+        if K > coin :
+            ans += K//coin
+            K %= coin
+        else :
+            continue
+    return ans
 
 print(solution(K))
