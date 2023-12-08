3210fd52f56d764ff2ff5ed8ef2e4749713e7f1f
ZerrY <suhan0304@gmail.com>
2023-12-08 21:49:08 +0900

    12904

--- 12904.py
+++ 12904.py
@@ -7,15 +7,17 @@ T = input().rstrip()
 
 # 반대로 올라가야됨 
 def make_string(st) :
-    if st == T :
+    if st == S :
         print(1)
         exit()
 
-    if len(st) < T :
+    if len(st) <= len(S) :
         print(0)
         exit()
 
     if st[-1] == 'A' :
         make_string(st[:-1])
     elif st[-1] == 'B' :
-        make_string(st[:0:-1])
+        make_string(st[-1::-1])
+
+make_string(T)
