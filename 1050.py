41bfad86f5b21a9258af1f4f9a29ac530b962004
ZerrY <suhan0304@gmail.com>
2023-12-09 19:25:21 +0900

    1050

--- 1050.py
+++ 1050.py
@@ -4,11 +4,17 @@ input = sys.stdin.readline
 
 N, M = map(int,input().rstrip().split())
 
+# input - 재료 & 비용
 ingre = dict() #재료
-
 for _ in range(N) :
     name, cost = input().split()
     ingre[name] = int(cost)
 
+# input - 조합식
+potion = dict() # 믈약
 for _ in range(M) :
-    
+    target, combination = input().split('=')
+    cost = 0
+    for comb in combination.split('+') :
+        cost += int(comb[0]) * ingre[comb[1:]]
+    ingre[
