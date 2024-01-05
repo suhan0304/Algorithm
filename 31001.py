faa5ae74b3717c271b1e71518c928b436d024fa1
ZerrY <suhan0304@gmail.com>
2024-01-05 15:42:52 +0900

    31001

--- 31001.py
+++ 31001.py
@@ -5,9 +5,11 @@ N, M, Q = map(int, input().rstrip().split())
 
 group = dict()
 price = dict()
+stock = dict()
 
 for _ in range(N) :
     G, H, P = input().rstrip().split()
+    price[H].append(int(P))
     if G in group :
         group[G].append(H)
     else :
@@ -16,4 +18,9 @@ for _ in range(N) :
 for _ in range(Q) :
     M, I1, I2 = input().rstrip().split()
     if M == "1" :
-        
+        if M >= price[I1] * int(I2) :
+            M -= price[I1] * int(I2)
+            if I1 in stock :
+                stock[I1] += int(I2)
+            else :
+                stock[I1] = int(I2)
