e4fd437ca176b396b3f227c373d730c791174e3b
ZerrY <suhan0304@gmail.com>
2024-01-05 15:45:42 +0900

    31001

--- 31001.py
+++ 31001.py
@@ -10,17 +10,19 @@ stock = dict()
 for _ in range(N) :
     G, H, P = input().rstrip().split()
     price[H].append(int(P))
+    stock[H] = 0
     if G in group :
         group[G].append(H)
     else :
         group[G] = [H]
 
 for _ in range(Q) :
-    M, I1, I2 = input().rstrip().split()
+    M, COMPANY, NUM = input().rstrip().split()
     if M == "1" :
-        if M >= price[I1] * int(I2) :
-            M -= price[I1] * int(I2)
-            if I1 in stock :
-                stock[I1] += int(I2)
-            else :
-                stock[I1] = int(I2)
+        if M >= price[COMPANY] * int(NUM) :
+            M -= price[COMPANY] * int(NUM)
+            stock[COMPANY] += int(NUM)
+    elif M == "2" :
+        if stock[COMPANY] > int(NUM) : 
+            stock[COMPANY] -= int(NUM)
+            M += int(NUM) * price[COMPANY]
