938ed7f63c0d0fd57b3d7ea7317e3c3d763928fb
ZerrY <suhan0304@gmail.com>
2024-01-05 15:48:05 +0900

    31001

--- 31001.py
+++ 31001.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline
 
-N, M, Q = map(int, input().rstrip().split())
+N, M, Q = map( inut().rstrip().split())
 
 group = dict()
 price = dict()
@@ -9,20 +9,25 @@ stock = dict()
 
 for _ in range(N) :
     G, H, P = input().rstrip().split()
-    price[H].append(int(P))
-    stock[H] = 0
+    price[H].append(P))    stock[H] = 0
     if G in group :
         group[G].append(H)
     else :
         group[G] = [H]
 
 for _ in range(Q) :
-    M, COMPANY, NUM = input().rstrip().split()
+    M, st, num = input().rstrip().split()
+    num = int(num)
     if M == "1" :
-        if M >= price[COMPANY] * int(NUM) :
-            M -= price[COMPANY] * int(NUM)
-            stock[COMPANY] += int(NUM)
+        if M >= price[st] * num :
+            M -= price[st] * num
+            stock[st] += num
     elif M == "2" :
-        if stock[COMPANY] > int(NUM) : 
-            stock[COMPANY] -= int(NUM)
-            M += int(NUM) * price[COMPANY]
+        if stock[st] > num : 
+            stock[st] -= num
+            M += num * price[st]
+    elif M == "3" :
+        price[st] += num
+    elif M == "4" :
+        for i in group[st] :
+            price[i] += num
