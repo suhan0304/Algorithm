66ef64aafca7ad046e02e692aa48615df610a66c
ZerrY <suhan0304@gmail.com>
2024-01-06 15:20:15 +0900

    31001

--- 31001.py
+++ 31001.py
@@ -3,7 +3,12 @@ input = sys.stdin.readline
 
 N, M, Q = map(int, input().rstrip().split())
 
+group = dict()
+price = dict()
 
 for _ in range(N) :
-
-
+    G, H, P = map(int, input().rstrip().split())
+    if G in group :
+        group[G].append(H)
+    else :
+        group[G] = [H]
