2e5839df7cf68f7c0b8885b191b775f830badc4e
ZerrY <suhan0304@gmail.com>
2024-01-06 15:21:45 +0900

    31001

--- 31001.py
+++ 31001.py
@@ -7,8 +7,11 @@ group = dict()
 price = dict()
 
 for _ in range(N) :
-    G, H, P = map(int, input().rstrip().split())
+    G, H, P = input().rstrip().split()
     if G in group :
         group[G].append(H)
     else :
         group[G] = [H]
+
+for _ in range(Q) :
+    M, I1, I2 = map(int, input().rstrip().split())
