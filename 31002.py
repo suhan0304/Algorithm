bfadd2282210c41f2f8943819bec38eccb6cb68d
ZerrY <suhan0304@gmail.com>
2024-01-07 17:58:13 +0900

    31002

--- 31002.py
+++ 31002.py
@@ -3,7 +3,12 @@ input = sys.stdin.readline
 
 N, K = map(int, input().rstrip().split())
 
-def make_graph(N) :
-    graph = [[] * N]
-    for i in range(N) :
-        graph[N] = 
+def complete_graph(N):
+    graph = [[0] * N for _ in range(N)]
+
+    for i in range(N):
+        for j in range(i + 1, N):
+            graph[i][j] = 1
+            graph[j][i] = 1
+
+    return graph
