18102e78ef48adb8b99f61b0272816081ffce642
ZerrY <suhan0304@gmail.com>
2024-01-08 20:10:36 +0900

    30827

--- 30827.py
+++ 30827.py
@@ -2,3 +2,11 @@ import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().rstrip().split())
+
+meeting = []
+for _ in range(N) :
+    meeting.append(tuple(map(int,input().rstrip().split())))
+
+sorted_meeting = sorted(meeting, key=lambda x: (x[0], x[1]))
+
+print(sorted_meeting)
