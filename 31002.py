ea77aac0c3c6079e0a830e1568a39cf784298bcf
ZerrY <suhan0304@gmail.com>
2024-01-08 19:19:39 +0900

    31002

--- 31002.py
+++ 31002.py
@@ -4,11 +4,3 @@ input = sys.stdin.readline
 NUM = (10**9) + 7
 
 N, K = map(int, input().rstrip().split())
-
-def solution(N, K):
-    for i in range(K) :
-        N = ((N-1) * 2) % NUM
-    
-    print(N)
-
-solution(N, K)
