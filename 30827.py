5e748a6ca8688be4f44c28ef2c6eb4045a7d6d6f
ZerrY <suhan0304@gmail.com>
2024-01-08 20:35:04 +0900

    30827

--- 30827.py
+++ 30827.py
@@ -9,13 +9,14 @@ for _ in range(N) :
 sorted_meetings = sorted(meetings, key=lambda x: (x[1], x[0]))
 #print(sorted_meetings)
 
-meeting_room = {i : 0 for i in range(1, K+1)}
+meeting_room = [0 for _ in range(K)]
 answer = 0
 
 for meeting in sorted_meetings :
     start_time, end_time = meeting
 
-    for room in range(1, K+1) :
+    meeting_room = sorted(meeting_room, reverse=True)
+    for room in range(K) :
         if start_time > meeting_room[room] :
             meeting_room[room] = end_time
             answer += 1
