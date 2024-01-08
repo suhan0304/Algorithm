11f7003bf14a59ebaff67881fc6a57879fcb0824
ZerrY <suhan0304@gmail.com>
2024-01-08 19:26:45 +0900

    31002

--- 31002.py
+++ 31002.py
@@ -4,3 +4,13 @@ input = sys.stdin.readline
 NUM = (10**9) + 7
 
 N, K = map(int, input().rstrip().split())
+
+if N == 3 :
+    print(3)
+    exit()
+
+edges = [N*(N-1)//2]
+neighborEdges = [(N-2)*2]
+
+print(edges)
+print(neighborEdges)
