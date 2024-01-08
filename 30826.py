8d7bcdfcaefd91965581f689811b8a1dfc612521
ZerrY <suhan0304@gmail.com>
2024-01-08 20:54:45 +0900

    30826

--- 30826.py
+++ 30826.py
@@ -1,16 +1,7 @@
 import sys
 input = sys.stdin.readline
 
-cnt = [9, 9]
-
 k = int(input().rstrip())
 
-s = 0
-i = 0
-while s < k :
-    s += cnt[i]
-    i += 1
-    if i == len(cnt) :
-        cnt.append(cnt[i-1] * 10)
-
-print(s)
+while True :
+    
