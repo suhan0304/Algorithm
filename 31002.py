88cd8c1c553723acbdf8c6bc14b31da6f670b89a
ZerrY <suhan0304@gmail.com>
2024-01-08 19:34:02 +0900

    31002

--- 31002.py
+++ 31002.py
@@ -8,13 +8,12 @@ N, K = map(int, input().rstrip().split())
 if N == 3 :
     print(3)
     exit()
-
 edges = N*(N-1)//2
 neighborEdges = (N-2)*2
 
 for _ in range(K-1) :
     edges = ((neighborEdges * edges) // 2) % NUM
-    print(_, edges)
+    #print(_, edges)
     neighborEdges = (neighborEdges-1) * 2
 
 print(edges % NUM)
