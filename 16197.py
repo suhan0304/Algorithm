fa432989b313a8e2083af35ec982dd6b99020375
ZerrY <suhan0304@gmail.com>
2024-01-09 17:18:16 +0900

    16197

--- 16197.py
+++ 16197.py
@@ -1,9 +1,20 @@
+from collections import deque
 import sys
+
 input = sys.stdin.readline
 
-N, M = map(int, input().rstrip().split())
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
-arr = []
-
-for _ in range(N) :
-    
+def bfs() :
+    while coin:
+        x1, y1, x2, y2, cnt = coin.popleft()
+        
+        if cnt >= 10 :
+            return -1
+        
+        for d in range(4) :
+            nx1 = x1 + dx[d]
+            ny1 = y1 + dy[d]
+            nx2 = x2 + dx[d]
+            ny2 = y2 + dy[d]
