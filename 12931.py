45e69a3521ac140ead5f461e8a52ace3a9b6a8a0
ZerrY <suhan0304@gmail.com>
2024-04-08 22:22:39 +0900

    12931

--- 12931.py
+++ 12931.py
@@ -6,14 +6,16 @@ a = list(map(int, input().split()))
 
 ans = 0
 while sum(a) > 0 :
-    print(a)
+    # remove odd number element in array
     for i in range(len(a)) :
         if a[i] % 2 == 1 :
             a[i] -= 1
             ans += 1
 
-        if a[i] != 0 :
-            a[i] /= 2
+    if sum(a) == 0 : break
+
+    # devide 2 all even number in array
+    a = [x / 2 for x in a]
     ans += 1
 
 print(ans)
