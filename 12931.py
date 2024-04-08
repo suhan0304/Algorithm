863f8b38d70894ef58c2a9060940e0378639af3a
ZerrY <suhan0304@gmail.com>
2024-04-08 22:15:51 +0900

    12931

--- 12931.py
+++ 12931.py
@@ -3,3 +3,17 @@ input = sys.stdin.readline
 
 N = int(input())
 a = list(map(int, input().split()))
+
+ans = 0
+while sum(a) > 0 :
+    print(a)
+    for i in range(len(a)) :
+        if a[i] % 2 == 1 :
+            a[i] -= 1
+            ans += 1
+
+        if a[i] != 0 :
+            a[i] /= 2
+    ans += 1
+
+print(ans)
