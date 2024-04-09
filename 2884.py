c0282fe87a907a191180168a9f70d682a413f8ff
ZerrY <suhan0304@gmail.com>
2024-04-09 15:29:35 +0900

    2884

--- 2884.py
+++ 2884.py
@@ -1,10 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-h, m = int(input().rstrip().split())
+h, m = map(int, input().rstrip().split())
 
 if m >= 45 :
     m -= 45
 else :
     h = h - 1 if h != 0 else 23
     m = m + 60 - 45 
+
+print(h, m)
