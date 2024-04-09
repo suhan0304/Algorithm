200be823745376fedbbb7491d0c801ff6d489bff
ZerrY <suhan0304@gmail.com>
2024-04-09 14:46:31 +0900

    2439

---
+++ 2439.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+a = int(input())
+
+for i in range(a) :
+    for j in range(a-(i+1)):
+        print(' ',end='')
+    for j in range(i+1) :
+        print('*',end='')
+    print()
