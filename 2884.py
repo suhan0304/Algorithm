86a581b1a18301cf00e5f9e076fea0cc3fc67d42
ZerrY <suhan0304@gmail.com>
2024-04-09 15:29:46 +0900

    2884

--- 2884.py
+++ 2884.py
@@ -3,8 +3,7 @@ input = sys.stdin.readline
 
 h, m = map(int, input().rstrip().split())
 
-if m >= 45 :
-    m -= 45
+if m >= 45 : m -= 45
 else :
     h = h - 1 if h != 0 else 23
     m = m + 60 - 45 
