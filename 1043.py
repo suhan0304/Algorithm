c72ef08e3ee162befd9ab75784d02b8a07ec2b54
ZerrY <suhan0304@gmail.com>
2024-04-29 16:39:34 +0900

    1043

--- 1043.py
+++ 1043.py
@@ -2,18 +2,3 @@ import sys
 input = sys.stdin.readline
 
 n,m = map(int, input().rstrip().split())
-
-member = set([i+1 for i in range(n)])
-truth = set()
-liar = set()
-
-line = list(map(int, input().rstrip().split()))
-for i in range(1, line[0]) :
-    truth.add(line[i])
-member = member - truth
-
-for i in range(m) :
-    line = list(map(int, input().rstrip().split()))
-    for i in range(1, line[0]) :
-        if line[i] in truth :
-            
