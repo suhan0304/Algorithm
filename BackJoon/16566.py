6399beb6eef67189020c89186c84a25d5b33ec1c
ZerrY <suhan0304@gmail.com>
2024-05-16 15:23:59 +0900

    16566

--- BackJoon/16566.py
+++ BackJoon/16566.py
@@ -7,14 +7,3 @@ card = list(map(int,input().rstrip().split()))
 card.sort()
 
 parent = [0 for _ in range(n+1)]
-
-def set_parent() :
-    for i in range(len(card)-1) :
-            parent[card[i]] = card[i+1]
-    
-    print(parent)
-    return parent
-
-set_parent()
-
-
