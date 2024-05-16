17e60c11ff98f440ebb8ba25c07dfaf1daeefb4c
ZerrY <suhan0304@gmail.com>
2024-05-16 15:22:29 +0900

    16566

--- BackJoon/16566.py
+++ BackJoon/16566.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 n, m, k = map(int, input().rstrip().split())
 
 card = list(map(int,input().rstrip().split()))
-card.sorted()
+card.sort()
 
 parent = [0 for _ in range(n+1)]
 
