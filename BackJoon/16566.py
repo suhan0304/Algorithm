23c93c0644bf9a7f96d8af51afd22c05efc93c39
ZerrY <suhan0304@gmail.com>
2024-05-16 15:22:13 +0900

    16566

---
+++ BackJoon/16566.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n, m, k = map(int, input().rstrip().split())
+
+card = list(map(int,input().rstrip().split()))
+card.sorted()
+
+parent = [0 for _ in range(n+1)]
+
+def set_parent() :
+    for i in range(len(card)-1) :
+            parent[card[i]] = card[i+1]
+    
+    print(parent)
+    return parent
+
+set_parent()
+
+
