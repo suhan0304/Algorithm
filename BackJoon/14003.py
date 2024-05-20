47de561e94f7c114866b4f1815515f133aad8f6a
ZerrY <suhan0304@gmail.com>
2024-05-20 15:38:13 +0900

    14003

--- BackJoon/14003.py
+++ BackJoon/14003.py
@@ -5,13 +5,26 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().rstrip().split()))
 
-dp = [arr[0]]
+LIS = [arr[0]]
+dp = [(0, arr[0])]
 
 for i in range(n) :
-    if dp[-1] < arr[i] :
-        dp.append(arr[i])
+    if LIS[-1] < arr[i] :
+        LIS.append(arr[i])
+        dp.append((len(LIS)-1, arr[i]))
     else :
-        idx = bisect.bisect_left(dp, arr[i])
-        dp[idx] = arr[i]
+        idx = bisect.bisect_left(LIS, arr[i])
+        LIS[idx] = arr[i]
+        dp.append((idx, arr[i]))
 
-print(len(dp))
+
+print(len(LIS))
+
+last_idx = len(LIS) - 1
+ans = []
+for i in range(len(dp)-1, -1, -1) :
+    if dp[i][0] == last_idx :
+        ans.append(dp[i][1])
+        last_idx -= 1
+
+print(*ans[::-1])
