e3ee550879c28edc5d6ecd8cb8c9943fc20ee5f4
ZerrY <suhan0304@gmail.com>
2024-05-24 17:25:58 +0900

    16234

--- BackJoon/16234.py
+++ BackJoon/16234.py
@@ -2,25 +2,21 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
+n, l, r = map(int, input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-n, l, r = map(int, input().split())
-graph = [list(map(int,input().split())) for _ in range(n)]
 visited = [[False for _ in range(n)] for _ in range(n)]
 
-def bfs(start_x, start_y) :
-    q = deque()
-    q.append((start_x, start_y))
-
-    sum = graph[start_x][start_y]
-    cnt = 1
+def bfs(sx, sy) :
+    u = []
+    q = deque((sx, sy))
+    u.append((sx, sy))
 
     while q :
         x, y = q.popleft()
-        graph[x][y] = -1
-        sum += graph[x][y]
-        cnt += 1
         visited[x][y] = True
 
         for d in range(4) :
@@ -30,6 +26,13 @@ def bfs(start_x, start_y) :
             if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] :
                 if l <= abs(graph[x][y] - graph[nx][ny]) <= r :
                     q.append((nx, ny))
+                    u.append((nx, ny))
+                    visited[nx][ny] = True
+
+    if len(u) <= 1 :
+        return 0
+    result = sum(graph[x][y] for x,y in u) // len(u)
+    for x, y in u :
     
     for i in range(n) :
         for j in range(n) :
