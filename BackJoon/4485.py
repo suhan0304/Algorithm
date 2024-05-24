771e4cc6ed750a212c95eef25e8d82c7bf75fff2
ZerrY <suhan0304@gmail.com>
2024-05-24 16:58:32 +0900

    4485

--- BackJoon/4485.py
+++ BackJoon/4485.py
@@ -13,7 +13,7 @@ def dijkstra() :
     dist[0][0] = 0
 
     while q :
-        d, x, y = heapq.heappop()
+        d, x, y = heapq.heappop(q)
 
         if x == t-1 and y == t-1 :
             print(f'Problem {cnt}: {dist[x][y]}')
@@ -30,7 +30,7 @@ def dijkstra() :
                     dist[nx][ny] = nd
                     heapq.heappush(q, (nd, nx, ny))
 
-cnt = 0
+cnt = 1
 while True :
     t = int(input())
     if t == 0 :
@@ -39,5 +39,5 @@ while True :
     graph = [list(map(int, input().split())) for _ in range(t)]
     dist = [[INF for _ in range(t)] for _ in range(t)]
 
-    dijkstra
+    dijkstra()
     cnt += 1
