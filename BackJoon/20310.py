3d7e9959c03117bcf5f077777ffc1d73cb395540
ZerrY <suhan0304@gmail.com>
2024-05-25 18:24:57 +0900

    20310

--- BackJoon/20310.py
+++ BackJoon/20310.py
@@ -1,4 +1,11 @@
 import sys
 input = sys.stdin.readline
 
-s = input().rstrip()
+s = list(input())
+cnt_0 = s.count('0')//2
+cnt_1 = s.count('1')//2
+for _ in range(cnt_0):
+    s.pop(-s[::-1].index('0')-1)
+for _ in range(cnt_1):
+    s.pop(s.index('1'))
+print(''.join(s))
