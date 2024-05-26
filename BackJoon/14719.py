1d4b5887464cd197023f45e6902e737809f5bad3
ZerrY <suhan0304@gmail.com>
2024-05-26 16:23:08 +0900

    14719

--- BackJoon/14719.py
+++ BackJoon/14719.py
@@ -3,5 +3,9 @@ input = sys.stdin.readline
 
 h, w = map(int, input().split())
 blocks = list(map(int, input().split()))
-arr = [[0 for _ in range(w)] for _ in range(h)]
- 
+
+ans = 0
+for i in range(1, w-1) :
+    l = max(blocks[:i])
+    r = max(blocks[i+1:])
+    
