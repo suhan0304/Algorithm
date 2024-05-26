f0950648699963ddf09bb67e1abda8398155eae3
ZerrY <suhan0304@gmail.com>
2024-05-26 17:45:16 +0900

    2138

--- BackJoon/2138.py
+++ BackJoon/2138.py
@@ -7,4 +7,19 @@ target = list(map(int, input()))
 
 before = cur[:]
 
-
+cnt = 0
+for i in range(2) :
+    cur[0] = (cur[0]+i)%2
+    cur[1] = (cur[0]+i)%2
+    for j in range(1, n) :
+        if cur[j-1] != target[j-1] :
+            cnt += 1
+            cur[j-1] = (cur[j-1]+1)%2
+            cur[j] = (cur[j]+1)%2
+            if j != n - 1 :
+                cur[j+1] = (cur[j+1]+1)%2
+    for i in range(n) :
+        if cur[i] != target[i] :
+            cur = before[:]
+            ans = 1
+            break
