d833822ce1dccd44624af07e65e0053338dab4e1
ZerrY <suhan0304@gmail.com>
2024-05-27 16:01:50 +0900

    24337

--- BackJoon/24337.py
+++ BackJoon/24337.py
@@ -7,20 +7,3 @@ n, a, b = map(int, input().split())
 if a + b - 1 > n :
     print(-1)
     exit(0)
-
-q_left = deque()
-q_right = deque()
-
-if a > b :
-
-elif a < b :
-    for i in range(1, a+1) :
-        q_left.append(i)
-    for j in range(1, b) :
-        q_right.append(j)
-
-    while q_right :
-        q_left.append(q_right.pop())
-else :
-
-print(*list(q_left))
