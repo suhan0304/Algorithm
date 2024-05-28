12b915253a64e8f0421de30126509c5832d0138b
ZerrY <suhan0304@gmail.com>
2024-05-28 09:46:55 +0900

    1943

--- BackJoon/1943.py
+++ BackJoon/1943.py
@@ -7,26 +7,22 @@ for __ in range(3) :
     total = 0
     for _ in range(n) :
         coin, cnt = map(int, input().split())
-        coins.append([coin,cnt])
-        total = coin * cnt
+        coins.append((coin,cnt))
+        total += coin * cnt
     
     if total%2 == 1:
         print(0)
         continue
+    total //= 2
 
     dp = [True] + [False] * total
-    for i in range(n) :
-        coin, cnt = coins[i]
+    for coin, cnt in coins :
 
         for n in range(total, coin-1, -1) :
             if dp[n-coin] :
                 for j in range(cnt) :
                     if n + coin * j <= total :
                         dp[n + coin * j] = True
-                    else :
-                        break
-        if dp[-1] :
-            print(1)
-            break
-    else :
-        print(0)
+                        
+
+    print(1 if dp[-1] else 0)
