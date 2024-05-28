88c01b034e7e199251a0ba4a9cf93723bc6ba830
ZerrY <suhan0304@gmail.com>
2024-05-28 09:38:42 +0900

    1943

--- BackJoon/1943.py
+++ BackJoon/1943.py
@@ -3,5 +3,30 @@ input = sys.stdin.readline
 
 for __ in range(3) :
     n = int(input())
-    coins = [list(map(int, input().split())) for _ in range(n)]
-    coins.sort()
+    coins = []
+    total = 0
+    for _ in range(n) :
+        coin, cnt = map(int, input().split())
+        coins.append([coin,cnt])
+        total = coin * cnt
+    
+    if total%2 == 1:
+        print(0)
+        continue
+
+    dp = [True] + [False] * total
+    for i in range(n) :
+        coin, cnt = coins[i]
+
+        for n in range(total, coin-1, -1) :
+            if dp[n-coin] :
+                for j in range(cnt) :
+                    if n + coin * j <= total :
+                        dp[n + coin * j] = True
+                    else :
+                        break
+        if dp[-1] :
+            print(1)
+            break
+    else :
+        print(0)
