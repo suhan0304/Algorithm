3ece34e9c4790625047aec77d870c7f9fa2a2a7a
ZerrY <suhan0304@gmail.com>
2024-05-30 10:02:49 +0900

    7812

--- BackJoon/7812.py
+++ BackJoon/7812.py
@@ -2,6 +2,20 @@ import sys
 sys.setrecursionlimit(10**4)
 input = sys.stdin.readline
 
+def dfs1(cur) :
+    visited[cur] = True
+    for v, w in graph[cur] :
+        if not visited[v] :
+            dfs1(v)
+            cnt[cur] += cnt[v]
+            dp[cur] += w * cnt[v] + dp[v]
+
+def dfs2(cur) :
+    visited[cur] = True
+    for v, w in graph[cur] :
+        if not visited[v] :
+            dp[v] = dp[cur] - cnt[v] * w + (n - cnt[v]) * w
+            dfs2(v)
 
 while True :
     n = (int(input()))
@@ -14,6 +28,13 @@ while True :
         graph[v].append((u, w))
         graph[u].append((v, w))
 
-    for i in range(n) :
-        val = 0
-        dp = [0] * (n+1)
+    val = 0
+    cnt = [1] * n
+    dp = [0] * n
+    visited = [False] * n
+    dfs1(0)
+
+    visited = [False] * n
+    dfs2(0)
+
+    print(min(dp))
