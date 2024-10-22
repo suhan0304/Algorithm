754a5c9166a4b66f9ff0c340091086682d16b956
ZerrY <suhan0304@gmail.com>
2024-10-22 17:31:32 +0900

    2024 KAKAO INTERNSHIP 3

---
+++ Programmers/2024KAKAO/3.py
@@ -0,0 +1,33 @@
+from itertools import combinations, product
+from bisect import bisect_left
+
+def solution(dice):
+    dp = {}
+    L = len(dice)
+
+    for A_index_combi in combinations(range(L), L//2) :
+        B_index_combi = [i for i in range(L) if i not in A_index_combi]
+
+        A, B = [], []
+        for order_dice in product(range(6), repeat=L//2) :
+            A.append(sum(dice[i][j] for i, j in zip(A_index_combi, order_dice)))
+            B.append(sum(dice[i][j] for i, j in zip(B_index_combi, order_dice)))
+
+        B.sort()
+
+        wins = 0
+        for num in A :
+            left, right = 0, len(B) - 1
+            while left <= right :
+                mid = (left + right) // 2
+                if num <= B[mid] :
+                    right = mid - 1
+                else :
+                    left = mid + 1
+            wins += left
+
+        dp[wins] = list(A_index_combi)
+
+    max_key = max(dp.keys())
+
+    return [x+1 for x in dp[max_key]]
